3fb8dbd498f45546e92d8f762147908a4afbd400
icuseven_cp <SND\icuseven_cp>
2011-11-17 17:25:30 +0000

    Preliminary scaffolding for Web Interfaces and publish functionality.

---
+++ Epi.Web.Interfaces/ISurveyRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Epi.Web.Interfaces
+{
+    
+        public class ISurveyRequest
+        {
+            public DateTime ClosingDate
+            {
+                get;
+                set;
+            }
+
+            public string SurveyName
+            {
+                get;
+                set;
+            }
+
+            public string OrganizationName
+            {
+                get;
+                set;
+            }
+
+            public string IntroductionText
+            {
+                get;
+                set;
+            }
+
+
+            public string TemplateXML
+            {
+                get;
+                set;
+            }
+
+        }
+
+
+    
+}
