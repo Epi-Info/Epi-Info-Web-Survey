bda9387df7ac021746bb53bb73a570941d162b50
icuseven_cp <SND\icuseven_cp>
2011-11-17 18:43:23 +0000

    Updated code format for better reading of Interface definitions.

--- Epi.Web.Interfaces/ISurveyRequestResult.cs
+++ Epi.Web.Interfaces/ISurveyRequestResult.cs
@@ -7,22 +7,8 @@ namespace Epi.Web.Interfaces
 {
     public interface ISurveyRequestResult
     {
-        public bool IsPulished
-        {
-            get;
-            set;
-        }
-
-        public string URL
-        {
-            get;
-            set;
-        }
-
-        public string StatusText
-        {
-            get;
-            set;
-        }
+        public bool IsPulished { get; set; }
+        public string URL { get; set; }
+        public string StatusText { get; set; }
     }
 }
