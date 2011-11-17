2401b185f2864ab2bb2ee36e51954812236d5600
icuseven_cp <SND\icuseven_cp>
2011-11-17 18:43:23 +0000

    Updated code format for better reading of Interface definitions.

--- Epi Info Web Survey/SurveyManagerInterface/Interfaces/IService.cs
+++ Epi Info Web Survey/SurveyManagerInterface/Interfaces/IService.cs
@@ -7,81 +7,43 @@ using System.Text;
 
 namespace SurveyManagerInterface
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface ISurveyManager
     {
-
         [OperationContract]
         ISurveyRequestResult PublishSurvey(ISurveyRequest pRequestMessage);
-
-
     }
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations
     [DataContract]
     public class ISurveyRequest : Epi.Web.Interfaces.ISurveyRequest
     {
+        [DataMember]
+        public DateTime ClosingDate {get;set;}
 
         [DataMember]
-        public DateTime ClosingDate
-        {
-            get;
-            set;
-        }
+        public string SurveyName { get; set; }
 
         [DataMember]
-        public string SurveyName
-        {
-            get;
-            set;
-        }
+        public string OrganizationName { get; set; }
 
         [DataMember]
-        public string OrganizationName
-        {
-            get;
-            set;
-        }
+        public string IntroductionText { get; set; }
 
         [DataMember]
-        public string IntroductionText
-        {
-            get;
-            set;
-        }
-
-
-        [DataMember]
-        public string TemplateXML
-        {
-            get;
-            set;
-        }
+        public string TemplateXML { get; set; }
 
     }
 
     public interface ISurveyRequestResult : Epi.Web.Interfaces.ISurveyRequestResult
     {
         [DataMember]
-        public bool IsPulished
-        {
-            get;
-            set;
-        }
+        public bool IsPulished { get; set; }
 
         [DataMember]
-        public string URL
-        {
-            get;
-            set;
-        }
+        public string URL { get; set; }
 
         [DataMember]
-        public string StatusText
-        {
-            get;
-            set;
-        }
+        public string StatusText { get; set; }
     }
 }
