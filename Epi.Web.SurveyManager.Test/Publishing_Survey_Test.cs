2dbd993ea80eb353656e5eaaca8b79b5c9248040
icuseven_cp <SND\icuseven_cp>
2011-11-21 18:17:43 +0000

    Added setup and premliminary test method to Epi.Web.BLL.Test

---
+++ Epi.Web.SurveyManager.Test/Publishing_Survey_Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Epi.Web.SurveyManager.Test
+{
+    [Category("Publisher")]
+    public class cPublishing_Survey
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Publish_The_survey()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
