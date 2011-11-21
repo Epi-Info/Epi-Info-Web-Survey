6030838daea1de98904ff2f9c9381199e6eda69b
icuseven_cp <SND\icuseven_cp>
2011-11-21 14:23:10 +0000

    Implemented cSurveyRequestResult in Epi.Web.BLL.

---
+++ EIWS_BLL_Core/cSurveyResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epi.Web.BLL
+{
+    public class cSurveyRequestResult : Epi.Web.Interfaces.ISurveyRequestResult
+    {
+        bool isPulished;
+        string uRL;
+        string statusText;
+
+        public bool IsPulished { get { return this.isPulished; } set { this.isPulished = value; } }
+
+        public string URL { get { return this.uRL; } set { this.uRL = value; } }
+
+        public string StatusText { get { return this.statusText; } set { this.statusText = value; } }
+    }
+}
