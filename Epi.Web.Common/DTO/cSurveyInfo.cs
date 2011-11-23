5534f82746f4a2a4a021c2edb937074ee6c3f3c7
TIsmail_cp <SND\TIsmail_cp>
2011-11-23 15:46:46 +0000

    created SurveyInfo DTO

---
+++ Epi.Web.Common/DTO/cSurveyInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epi.Web.Common.DTO
+{
+    class cSurveyInfo
+    {
+        private string _SurveyId;
+        private string _SurveyNumber;
+        private string _SurveyName;
+        private string _SurveyDescription;
+        private string _XML;
+        private bool _IsSuccess;
+
+
+        public string SurveyId
+        {
+            get { return _SurveyId; }
+            set { _SurveyId = value; }
+        }
+        public string SurveyNumber
+        {
+            get { return _SurveyNumber; }
+            set { _SurveyNumber = value; }
+        }
+
+        public string SurveyName
+        {
+            get { return _SurveyName; }
+            set { _SurveyName = value; }
+        }
+
+        public string SurveyDescription
+        {
+            get { return _SurveyDescription; }
+            set { _SurveyDescription = value; }
+        }
+        public string XML
+        {
+            get { return _XML; }
+            set { _XML = value; }
+        }
+
+        public bool IsSuccess
+        {
+            get { return _IsSuccess; }
+            set { _IsSuccess = value; }
+        }
+
+    }
+}
