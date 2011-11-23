8261bd845a126346b8a4bdcdeff101468c461b34
icuseven_cp <SND\icuseven_cp>
2011-11-23 15:50:48 +0000

    Updated DTO WCF Contract attributes.

--- Epi.Web.Common/DTO/cSurveyInfo.cs
+++ Epi.Web.Common/DTO/cSurveyInfo.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace Epi.Web.Common.DTO
 {
-    class cSurveyInfo
+    [DataContract]
+    public class cSurveyInfo
     {
         private string _SurveyId;
         private string _SurveyNumber;
@@ -15,34 +17,39 @@ namespace Epi.Web.Common.DTO
         private bool _IsSuccess;
 
 
+        [DataMember]
         public string SurveyId
         {
             get { return _SurveyId; }
             set { _SurveyId = value; }
         }
+        [DataMember]
         public string SurveyNumber
         {
             get { return _SurveyNumber; }
             set { _SurveyNumber = value; }
         }
 
+        [DataMember]
         public string SurveyName
         {
             get { return _SurveyName; }
             set { _SurveyName = value; }
         }
 
+        [DataMember]
         public string SurveyDescription
         {
             get { return _SurveyDescription; }
             set { _SurveyDescription = value; }
         }
+        [DataMember]
         public string XML
         {
             get { return _XML; }
             set { _XML = value; }
         }
-
+        [DataMember]
         public bool IsSuccess
         {
             get { return _IsSuccess; }
