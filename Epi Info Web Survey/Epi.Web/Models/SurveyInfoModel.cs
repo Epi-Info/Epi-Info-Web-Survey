6c0d7cf67e7b28c220cb08f18f4322dc73c96061
TIsmail_cp <SND\TIsmail_cp>
2011-12-02 19:38:21 +0000

    SurveyModel maps objects on MVC side

---
+++ Epi Info Web Survey/Epi.Web/Models/SurveyInfoModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epi.Web.Models
+{
+    
+    /// <summary>
+    /// The Survey Model that will be pumped to view
+    /// </summary>
+    public class SurveyInfoModel
+    {
+        private string _SurveyId;
+        private string _SurveyNumber;
+        private string _SurveyName;
+        private string _IntroductionText;
+        private string _DepartmentName;
+        private string _OrganizationName;
+        private string _XML;
+        private bool _IsSuccess;
+
+
+        public string SurveyId
+        {
+            get { return _SurveyId; }
+            set { _SurveyId = value; }
+        }
+        
+        public string SurveyNumber
+        {
+            get { return _SurveyNumber; }
+            set { _SurveyNumber = value; }
+        }
+
+        
+        public string SurveyName
+        {
+            get { return _SurveyName; }
+            set { _SurveyName = value; }
+        }
+
+        
+        public string OrganizationName
+        {
+            get { return _OrganizationName; }
+            set { _OrganizationName = value; }
+        }
+
+        
+        public string DepartmentName
+        {
+            get { return _DepartmentName; }
+            set { _DepartmentName = value; }
+        }
+
+
+        
+        public string IntroductionText
+        {
+            get { return _IntroductionText; }
+            set { _IntroductionText = value; }
+        }
+        
+        public string XML
+        {
+            get { return _XML; }
+            set { _XML = value; }
+        }
+        
+        public bool IsSuccess
+        {
+            get { return _IsSuccess; }
+            set { _IsSuccess = value; }
+        }
+
+    }
+}
