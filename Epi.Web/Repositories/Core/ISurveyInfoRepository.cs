a3cf399d30257f372747e61f01a287c7c6ffd3b7
TIsmail_cp <SND\TIsmail_cp>
2011-12-02 19:39:18 +0000

    Implementing Repository pattern on MVC end

---
+++ Epi.Web/Repositories/Core/ISurveyInfoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epi.Web.Repositories.Core
+{
+    /// <summary>
+    /// SurveyInfo Repository interface.
+    /// Derives from standard IRepository. Adds one SurveyInfo specific member.
+    /// </summary>
+    public interface ISurveyInfoRepository: IRepository<Epi.Web.Common.DTO.SurveyInfoDTO>
+    {
+        Epi.Web.Common.DTO.SurveyInfoDTO GetSurveyInfoById(string pId);
+    }
+}
