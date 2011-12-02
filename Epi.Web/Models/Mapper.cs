d0f3413850866858b0665c3c0b8e69b28ae6e73d
TIsmail_cp <SND\TIsmail_cp>
2011-12-02 19:37:57 +0000

    Mapping surveyDto object to surveyModel object

---
+++ Epi.Web/Models/Mapper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Epi.Web.Models;
+namespace Epi.Web.Models
+{
+    /// <summary>
+    /// Maps DTO object to Model object or Model object to DTO object
+    /// </summary>
+    public static class Mapper
+    {
+
+        /// <summary>
+        /// Maps SurveyInfo DTO to SurveyInfo MVC.
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
+
+       
+        public static SurveyInfoModel ToSurveyInfoModel(this Epi.Web.Common.DTO.SurveyInfoDTO SurveyInfoDTO)
+        {
+            return new SurveyInfoModel
+            {
+                SurveyId = SurveyInfoDTO.SurveyId,
+                SurveyNumber = SurveyInfoDTO.SurveyNumber,
+                SurveyName = SurveyInfoDTO.SurveyName,
+                OrganizationName = SurveyInfoDTO.OrganizationName,
+                DepartmentName = SurveyInfoDTO.DepartmentName,
+                IntroductionText = SurveyInfoDTO.IntroductionText,
+                XML = SurveyInfoDTO.XML,
+                IsSuccess = SurveyInfoDTO.IsSuccess
+            };
+        }
+
+        /// <summary>
+        /// Maps SurveyInfo Model to SurveyInfo DTO.
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
+
+        public static Epi.Web.Common.DTO.SurveyInfoDTO ToSurveyInfoDTO(SurveyInfoModel SurveyInfoModel)
+        {
+            return new Epi.Web.Common.DTO.SurveyInfoDTO
+            {
+                SurveyId = SurveyInfoModel.SurveyId,
+                SurveyNumber = SurveyInfoModel.SurveyNumber,
+                SurveyName = SurveyInfoModel.SurveyName,
+                OrganizationName = SurveyInfoModel.OrganizationName,
+                DepartmentName = SurveyInfoModel.DepartmentName,
+                IntroductionText = SurveyInfoModel.IntroductionText,
+                XML = SurveyInfoModel.XML,
+                IsSuccess = SurveyInfoModel.IsSuccess
+            };
+        }
+
+    }
+}
