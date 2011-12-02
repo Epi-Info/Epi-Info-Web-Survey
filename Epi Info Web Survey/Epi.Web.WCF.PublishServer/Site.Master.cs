e8e78d8ac685246b92e3e734568660706412b4a8
mlamtahri_cp <SND\mlamtahri_cp>
2011-12-02 15:42:02 +0000

    changes to Service Host

--- Epi Info Web Survey/Epi.Web.WCF.PublishServer/Site.Master.cs
+++ Epi Info Web Survey/Epi.Web.WCF.PublishServer/Site.Master.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Epi.Web.WCF.PublishServer
+namespace Epi.Web.WCF.SurveyHost
 {
     public partial class SiteMaster : System.Web.UI.MasterPage
     {
