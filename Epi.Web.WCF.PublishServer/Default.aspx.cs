60a70002cc4a4e4f3521c680d94c776d5486c341
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2011-12-02 15:42:02 +0000

    changes to Service Host

--- Epi.Web.WCF.PublishServer/Default.aspx.cs
+++ Epi.Web.WCF.PublishServer/Default.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Epi.Web.WCF.PublishServer
+namespace Epi.Web.WCF.SurveyHost
 {
     public partial class _Default : System.Web.UI.Page
     {
