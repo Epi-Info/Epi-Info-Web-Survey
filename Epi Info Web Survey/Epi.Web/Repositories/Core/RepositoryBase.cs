eb69794e2c05021cc27947f0d15d9c59c4be5546
TIsmail_cp <SND\TIsmail_cp>
2011-12-02 19:39:18 +0000

    Implementing Repository pattern on MVC end

---
+++ Epi Info Web Survey/Epi.Web/Repositories/Core/RepositoryBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.ServiceModel;
+using Epi.Web.ActionServiceClient;
+namespace Epi.Web.Repositories.Core
+{
+    /// <summary>
+    /// Base class for all Repositories. 
+    /// Provides common repository functionality, including:
+    ///   - Management of Service client.
+    ///   - Offers common request-response correlation check.
+    /// </summary>
+    public abstract class RepositoryBase
+    {
+        /// <summary>
+        /// Lazy loads ActionServiceClient and stores it in Session object.
+        /// </summary>
+        protected SurveyManagerClient Client
+        {
+            get
+            {
+                // Check if not initialized yet
+                if (HttpContext.Current.Session["ActionServiceClient"] == null)
+                    HttpContext.Current.Session["ActionServiceClient"] = new SurveyManagerClient();
+
+                // If current client is 'faulted' (due to some error), create a new instance.
+                var client = HttpContext.Current.Session["ActionServiceClient"] as SurveyManagerClient;
+                if (client.State == CommunicationState.Faulted)
+                {
+                    try { client.Abort(); }
+                    catch { /* no action */ }
+
+                    client = new SurveyManagerClient();
+                    HttpContext.Current.Session["ActionServiceClient"] = client;
+                }
+
+                return client;
+            }
+        }
+
+        /// <summary>
+        /// Correlates requestid with returned response correlationId. 
+        /// These must always match. If not, request and responses are not related.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="response"></param>
+        //protected void Correlate(RequestBase request, ResponseBase response)
+        //{
+        //    if (request.RequestId != response.CorrelationId)
+        //        throw new ApplicationException("RequestId and CorrelationId do not match.");
+        //}
+    }
+}
