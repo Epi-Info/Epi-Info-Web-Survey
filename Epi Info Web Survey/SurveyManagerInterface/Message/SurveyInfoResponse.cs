e1c4015431e980a26f416aade843f15759fb6b52
icuseven_cp <SND\icuseven_cp>
2011-12-06 16:24:11 +0000

    Replaced Customer references in the SurveyInfo files. Deleted the Epi.Web.dll and Epi.Web.pdb from source code to prevent build errors.

--- Epi Info Web Survey/SurveyManagerInterface/Message/SurveyInfoResponse.cs
+++ Epi Info Web Survey/SurveyManagerInterface/Message/SurveyInfoResponse.cs
@@ -7,30 +7,30 @@ using Epi.Web.Common.DTO;
 namespace Epi.Web.WCF.SurveyService.Messages
 {
     /// <summary>
-    /// Represents a customer response message to client
+    /// Represents a SurveyInfo response message to client
     /// </summary>    
     [DataContract(Namespace = "http://www.yourcompany.com/types/")]
     public class SurveyInfoResponse : ResponseBase
     {
         /// <summary>
-        /// Default Constructor for CustomerResponse.
+        /// Default Constructor for SurveyInfoResponse.
         /// </summary>
         public SurveyInfoResponse() { }
 
         /// <summary>
-        /// Overloaded Constructor for CustomerResponse. Sets CorrelationId.
+        /// Overloaded Constructor for SurveyInfoResponse. Sets CorrelationId.
         /// </summary>
         /// <param name="correlationId"></param>
         public SurveyInfoResponse(string correlationId) : base(correlationId) { }
 
         /// <summary>
-        /// List of customers. 
+        /// List of SurveyInfos. 
         /// </summary>
         [DataMember]
         public IList<SurveyInfoDTO> SurveyInfoList;
 
         /// <summary>
-        /// Single customer
+        /// Single SurveyInfo
         /// </summary>
         [DataMember]
         public SurveyInfoDTO SurveyInfo;
