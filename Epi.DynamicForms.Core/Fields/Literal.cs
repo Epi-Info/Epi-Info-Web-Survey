f462ecfacfa55b1894f707a2c7be58adb311c33f
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2011-12-12 16:37:58 +0000

    Added TextBox to survey

--- Epi.DynamicForms.Core/Fields/Literal.cs
+++ Epi.DynamicForms.Core/Fields/Literal.cs
@@ -38,8 +38,8 @@ namespace MvcDynamicForms.Fields
 
                
                 StringBuilder StyleValues = new StringBuilder();
-              
-                StyleValues.Append(GetContolStyle(_fontstyle.ToString()));
+
+                StyleValues.Append(GetContolStyle(_fontstyle.ToString(), _top.ToString(), _left.ToString(), Width.ToString(), Height.ToString()));
 
                 wrapper.Attributes.Add(new KeyValuePair<string, string>("style", StyleValues.ToString()));  
                 wrapper.InnerHtml = Html;
@@ -47,90 +47,6 @@ namespace MvcDynamicForms.Fields
             }
             return Html;
         }
-        private string GetContolStyle(string ControlFontStyle)
-        {
-
-            StringBuilder FontStyle  = new StringBuilder();
-            StringBuilder  FontWeight  = new StringBuilder();
-            StringBuilder  TextDecoration = new StringBuilder();
-            StringBuilder CssStyles = new StringBuilder();
-
-            char[] delimiterChars = { ' ', ',' };
-            string[] Styles = ControlFontStyle.Split(delimiterChars);
-
-            CssStyles.Append("position:absolute;left:" + this._left.ToString() +
-                    "px;top:" + this._top.ToString() + "px" + ";width:" + Width.ToString() + "px" + ";Height:" + Height.ToString() + "px");
-
-                  
-            foreach (string Style in Styles)
-            {
-                  switch (Style.ToString())
-                {
-                    case "Italic":
-                        FontStyle.Append(Style.ToString());
-                        break;
-                    case "Oblique":
-                        FontStyle.Append(Style.ToString());
-
-                        break;
-                  
-                }
-
-            }
-            foreach (string Style in Styles)
-            {
-                switch (Style.ToString())
-                {
-                    case "Bold":
-                        FontWeight.Append(Style.ToString());
-                        break;
-                    case "Normal":
-                        FontWeight.Append(Style.ToString());
-
-                        break;
-
-                }
-
-            }
-             CssStyles.Append(";font:");//1
-           if (!string.IsNullOrEmpty(FontStyle.ToString())) 
-            {
-               
-                CssStyles.Append(FontStyle);//2
-                CssStyles.Append(" ");//3
-            }
-            CssStyles.Append(FontWeight);
-            CssStyles.Append(" ");
-            CssStyles.Append(_fontSize.ToString() + "pt ");
-            CssStyles.Append(" ");
-            CssStyles.Append(_fontfamily.ToString());
-
-            foreach (string Style in Styles)
-            {
-                switch (Style.ToString())
-                {
-                    case "Strikeout":
-                        TextDecoration.Append("line-through");
-                        break;
-                    case "Underline":
-                        TextDecoration.Append(Style.ToString());
-
-                        break;
-
-                }
-
-            }
-            if (!string.IsNullOrEmpty(TextDecoration.ToString()))
-            
-            {
-                CssStyles.Append(";text-decoration:");
-            }
-            
-            CssStyles.Append(TextDecoration);
-
-
-            return CssStyles.ToString();
-
-        }
+   
     }
 }
