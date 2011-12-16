2a62d0e5a25651b10829fe016a83a41e1f089050
TIsmail_cp <SND\TIsmail_cp>
2011-12-16 18:17:01 +0000

    Checked in the changes for readonly property

--- Epi.DynamicForms.Core/Fields/TextBox.cs
+++ Epi.DynamicForms.Core/Fields/TextBox.cs
@@ -60,6 +60,15 @@ namespace MvcDynamicForms.Fields
             txt.MergeAttributes(_inputHtmlAttributes);
             html.Append(txt.ToString(TagRenderMode.SelfClosing));
 
+            // If readonly then add the following jquery script to make the field disabled 
+            if (ReadOnly)
+            {
+                var scriptReadOnlyText = new TagBuilder("script");
+                scriptReadOnlyText.InnerHtml = "$(function(){$('#" + inputName + "').attr('disabled','disabled')});";
+                html.Append(scriptReadOnlyText.ToString(TagRenderMode.Normal));
+            }
+
+
             var wrapper = new TagBuilder(_fieldWrapper);
             wrapper.Attributes["class"] = _fieldWrapperClass;
             wrapper.InnerHtml = html.ToString();
