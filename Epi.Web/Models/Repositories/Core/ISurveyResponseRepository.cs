c0fdeb704cfc186421f9670bf0b1b4a9c315e491
icuseven_cp <SND\icuseven_cp>
2011-12-16 15:50:49 +0000

    Wired up the SurveyResponseXML and added Repository to MVC project.

---
+++ Epi.Web/Models/Repositories/Core/ISurveyResponseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Epi.Web.Common.Message;
+
+namespace Epi.Web.Repositories.Core
+{
+    /// <summary>
+    /// SurveyResponse Repository interface.
+    /// Derives from standard IRepository of SurveyResponseResponse. Adds a method GetSurveyResponse .
+    /// </summary>
+    public interface ISurveyResponseRepository: IRepository<Epi.Web.Common.Message.SurveyResponseResponse>
+    {
+        SurveyResponseResponse GetSurveyResponse(SurveyResponseRequest pRequest);
+        SurveyResponseResponse SaveSurveyResponse(SurveyResponseRequest pRequest);
+    }
+}
