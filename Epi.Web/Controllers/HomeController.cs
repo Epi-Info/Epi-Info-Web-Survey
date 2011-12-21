238eca4eefe405aedcca306cd351dd7f9fbb0e1d
TIsmail_cp <SND\TIsmail_cp>
2011-12-21 15:11:22 +0000

    refactoring code. created home, survey and final controller to implement seperation of concern. Implemented mvc, implementing unity container pattern

--- Epi.Web/Controllers/HomeController.cs
+++ Epi.Web/Controllers/HomeController.cs
@@ -7,48 +7,35 @@ using Epi.Web.Models;
 using Epi.Web.Repositories.Core;
 using Epi.Web.Common.Message;
 using Epi.Web.Common.Criteria;
-
+using Epi.Web.Models.Constants;
+using MvcDynamicForms.Demo.Models;
+using Epi.Web.Models.Utility;
 
 namespace Epi.Web.Controllers
 {
     public class HomeController : Controller
     {
-        
-        
-        // Initialize ISurveyInfoRepository 
+
+        // declare ISurveyInfoRepository which inherits IRepository of SurveyInfoResponse object
         private ISurveyInfoRepository _iSurveyInfoRepository;
-        //Initialize SurveyInfoRequest
+
+        //declare SurveyInfoRequest
         private Epi.Web.Common.Message.SurveyInfoRequest _surveyInfoRequest;
 
-
-        private ISurveyAnswerRepository _iSurveyResponseRepository;
-        private Epi.Web.Common.Message.SurveyAnswerRequest _surveyResponseRequest;
-
-
+        
         /// <summary>
-        /// Injectinting ISurveyInfoRepository through Constructor
+        /// Injectinting ISurveyInfoRepository and SurveyInfoRequest through Constructor
         /// </summary>
         /// <param name="iSurveyInfoRepository"></param>
-        public HomeController(ISurveyInfoRepository iSurveyInfoRepository, Epi.Web.Common.Message.SurveyInfoRequest surveyInfoRequest, ISurveyAnswerRepository iSurveyResponseRepository)
+        public HomeController(ISurveyInfoRepository iSurveyInfoRepository,
+                                  Epi.Web.Common.Message.SurveyInfoRequest surveyInfoRequest)
         {
             _iSurveyInfoRepository = iSurveyInfoRepository;
             _surveyInfoRequest = surveyInfoRequest;
-            _iSurveyResponseRepository = iSurveyResponseRepository;
-        }
-
-        [HttpGet]
-        public ActionResult Index()
-        {
-          
-            return View("Index");
         }
 
 
-        
-        public ActionResult Index1()
-        {
-            return View("Index");
-        }
+
         /// <summary>
         /// Accept SurveyId as parameter, 
         /// 
@@ -58,133 +45,38 @@ namespace Epi.Web.Controllers
         /// <param name="surveyid"></param>
         /// <returns></returns>
         [HttpGet]
-        public ActionResult ListSurvey(string surveyid)
+        public ActionResult Index(string surveyid)
         {
             /*TO DO: Later we will pass an empty SurveyInfoDTO object and validate it here something like
              if surveyInfodto.SurveyName== null then go to the exception page*/
             try
             {
-               
+
                 _surveyInfoRequest.Criteria.SurveyId = surveyid;
                 SurveyInfoResponse surveyInfoResponse = _iSurveyInfoRepository.GetSurveyInfo(_surveyInfoRequest);
                 var s = Mapper.ToSurveyInfoModel(surveyInfoResponse.SurveyInfo);
 
-                return View("SurveyIntroduction", s);
+                return View(Epi.Web.Models.Constants.Constant.INDEX_PAGE, s);
             }
             catch (Exception ex)
             {
-                return View("Exception");
+                return View(Epi.Web.Models.Constants.Constant.EXCEPTION_PAGE);
             }
         }
 
-        
-       /// <summary>
-       /// Assign the SurveyId to the surveyInforequest object
-        /// get the surveyInfoDTO based on surveyInforequest object by method GetSurveyInfo(SurveyInfoRequest)
-       /// Get the form object based on SurveyInfoDTO and pass to the view
-       /// Model Binding
-       /// </summary>
-       /// <param name="surveyModel"></param>
-       /// <returns></returns>
+        /// <summary>
+        /// redirecting to Survey controller to action method Index
+        /// </summary>
+        /// <param name="surveyModel"></param>
+        /// <returns></returns>
         [HttpPost]
-        public ActionResult ListSurvey(Epi.Web.Models.SurveyInfoModel surveyModel, string submit, string surveyid)
+        public ActionResult Index(Epi.Web.Models.SurveyInfoModel surveyModel)
         {
-            //SurveyInfoRequest surveyInfoRequest = new SurveyInfoRequest();
-            _surveyInfoRequest.Criteria.SurveyId = surveyModel.SurveyId;
-            var surveyInfoDTO = _iSurveyInfoRepository.GetSurveyInfo(_surveyInfoRequest).SurveyInfo;
-            var form = MvcDynamicForms.Demo.Models.FormProvider.GetForm(surveyInfoDTO, this.GetCurrentPage(), this.GetCurrentSurveyAnswer());
             
-
-            if (!string.IsNullOrEmpty(submit))
-            {
-                return Submit(surveyid, surveyModel);
-            }
-            else
-            {
-                SurveyAnswerRequest surveyResponseRequest = new SurveyAnswerRequest();
-                Guid ResponseID = Guid.NewGuid();
-                TempData["SurveyResponseId"] = ResponseID.ToString();
-                surveyResponseRequest.Criteria.ResposneId = ResponseID.ToString();
-                
-                surveyResponseRequest.SurveyResponseDTO = new Common.DTO.SurveyAnswerDTO();
-                surveyResponseRequest.SurveyResponseDTO.ResponseId = ResponseID.ToString();
-                surveyResponseRequest.SurveyResponseDTO.DateCompleted = DateTime.MinValue ;
-                surveyResponseRequest.SurveyResponseDTO.SurveyId = surveyid;
-                surveyResponseRequest.SurveyResponseDTO.Status = 1;
-                surveyResponseRequest.SurveyResponseDTO.XML = new SurveyResponseXML(surveyid).CreateResponseXml().InnerXml;
-                surveyResponseRequest.Action = "Create";
-                SurveyAnswerResponse surveyResponseResponse =  _iSurveyResponseRepository.SaveSurveyAnswer(surveyResponseRequest);
-
-                return View("Survey", form);
-            }
-        }
-
-        public ActionResult Submit(string surveyid, Epi.Web.Models.SurveyInfoModel surveyModel)
-        {
-            _surveyInfoRequest.Criteria.SurveyId = surveyModel.SurveyId;
-            var surveyInfoDTO = _iSurveyInfoRepository.GetSurveyInfo(_surveyInfoRequest).SurveyInfo;
-
-            var form = MvcDynamicForms.Demo.Models.FormProvider.GetForm(surveyInfoDTO, this.GetCurrentPage(), this.GetCurrentSurveyAnswer());// Requesting the first page of the survey.
+            return RedirectToAction(Epi.Web.Models.Constants.Constant.INDEX, Epi.Web.Models.Constants.Constant.SURVEY_CONTROLLER);
             
-            _surveyInfoRequest.Criteria.SurveyId = surveyid;
-            SurveyInfoResponse surveyInfoResponse = _iSurveyInfoRepository.GetSurveyInfo(_surveyInfoRequest);
-            var s = Mapper.ToSurveyInfoModel(surveyInfoResponse.SurveyInfo);
-            UpdateModel(form);
-            if (form.Validate())
-            {
-                // 1 Get the record for the current survey response
-                // 2 update the current survey response
-                // 3 save the current survey response
-
-                // 1 Get the record for the current survey response
-                SurveyAnswerRequest surveyResponseRequest = new SurveyAnswerRequest();
-                surveyResponseRequest.Criteria.ResposneId = TempData["SurveyResponseId"].ToString();
-                SurveyAnswerResponse surveyResponseResponse = _iSurveyResponseRepository.GetSurveyAnswer(surveyResponseRequest);
-
-                // 2 update the current survey response
-                surveyResponseRequest.SurveyResponseDTO = surveyResponseResponse.SurveyResponseDTO;
-                SurveyResponseXML surveyXML = new SurveyResponseXML(surveyid);
-                surveyXML.Add(form);
-                surveyResponseRequest.SurveyResponseDTO.XML = surveyXML.CreateResponseXml().InnerXml;
-
-                // 3 save the current survey response
-                surveyResponseRequest.Action = "Update";
-                surveyResponseResponse = _iSurveyResponseRepository.SaveSurveyAnswer(surveyResponseRequest);
-
-              return View("PostSubmit", s);
-               // form = MvcDynamicForms.Demo.Models.FormProvider.GetForm(surveyInfoDTO, this.GetCurrentPage(), this.GetCurrentSurveyAnswer());//Testing SurveyAnswer 
-               // return View("Survey", form); //Testing SurveyAnswer 
-            }
-            else
-            {
-                return View("Survey", form);
-            }
         }
 
-        private int GetCurrentPage()
-        {
-            int CurrentPage = 1;
-            if (TempData.ContainsKey("CurrentPage") && TempData["CurrentPage"] != null)
-            {
-                int.TryParse(TempData["CurrentPage"].ToString(), out CurrentPage);
-            }
-
-            return CurrentPage;
-        }
-
-        private Epi.Web.Common.DTO.SurveyAnswerDTO GetCurrentSurveyAnswer()
-        {
-            Epi.Web.Common.DTO.SurveyAnswerDTO result = null;
-
-            if (TempData.ContainsKey("SurveyResponseId") && TempData["SurveyResponseId"] != null && !string.IsNullOrEmpty(TempData["SurveyResponseId"].ToString()))
-            {
-                SurveyAnswerRequest surveyAnswerRequest = new SurveyAnswerRequest();
-                surveyAnswerRequest.Criteria.ResposneId = TempData["SurveyResponseId"].ToString();
-                result = _iSurveyResponseRepository.GetSurveyAnswer(surveyAnswerRequest).SurveyResponseDTO;
-            }
-
-            return result;
-        }
 
     }
 }
---
+++ Epi.Web/Views/Survey/Index.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Title="Enter Survey" Language="C#" MasterPageFile="~/Views/Shared/EpiSurvey.Master"
+    Inherits="System.Web.Mvc.ViewPage<MvcDynamicForms.Form>" %>
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
+    Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+ 
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    Index
+</asp:Content>
+ <asp:Content ID="Content2" ContentPlaceHolderID="HeaderContent" runat="server">
+     <div id="header">
+            <h1>
+                Survey Name- <%:Model.SurveyInfo.SurveyName %>
+                </h1>
+        </div>
+</asp:Content>
+ 
+ <asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+     
+    <div id="infobox">
+        <div class="pages">
+		<span class="nextprev"><img src="/Content/images/prev_d.png" height="16px" style="vertical-align:text-top;" alt="Previous Page"></span>
+		<span class="current">1</span>
+		<a href="" title="Go to page 2">2</a>
+		<a href="" title="Go to page 3">3</a>
+		<a href="" title="Go to page 4">4</a>
+		<a href="" title="Go to page 5">5</a>
+		<a href="" title="Go to page 6">6</a>
+		<a href="" title="Go to page 7">7</a>
+		<a href="" title="Go to page 8">8</a>
+		<a href="/page9" title="Go to page 9">9</a>
+		<a href="/page10" title="Go to page 10">10</a>
+	    <span>&#8230;</span><a href="/page230" title="Go to page 15">15</a>
+		<a href="/page231" title="Go to page 20">20</a>
+		<a href="/page2" class="nextprev" title="Go to Next Page"><img src="/Content/images/next.png" height="16px" style="vertical-align: text-top; text-decoration:none;" border="0" alt="Next Page"></a>
+	  </div>
+	   
+	  <div id="exit">
+      <button class="exitsurvey" type="submit">Exit Survey</button>
+      
+      </div>
+	  <div style="clear:both;"></div>
+	 </div>
+
+	<div id="content">
+		 
+
+        <%Html.BeginForm(null , null, FormMethod.Post, new { id = "myform" }); %>
+        
+        <%=Model.RenderHtml(true) %>
+        
+      
+   	</div>
+	        <div id="nav">
+
+		        
+                  <div id="prev" align="left"><button class="prev"  type="submit" ><img class="button" src="../../Content/images/arrow_left.png"> Previous</button></div>
+	 		 <div id="save" align="center"><button class="save" type="submit" onclick="window.location='survey3.html'"><img class="button" src="../../Content/images/save.png"> Save & Close</button></div>
+	 		 
+          <div id="next" align="right"><button class="submits" type="submit"  name="submit" value="Submit" >
+              Submit Survey </button></div> 
+
+            
+	        </div>
+
+
+
+
+ <%Html.EndForm(); %>
+</asp:Content>
