edd8321617e66770294356c88848fe7240c10a5c
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2011-12-27 19:03:38 +0000

    Refactor FormProvider  + Unit Tests

---
+++ Epi.Web/Utility/LabelProvider.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using MvcDynamicForms.Fields;
+using System.Xml;
+using System.Xml.Linq;
+using System.Text;
+using MvcDynamicForms;
+namespace Epi.Web.MVC.Utility
+{
+    public class LabelProvider
+    {
+        public Literal GetLabel(XElement _FieldTypeID, double _Width, double _Height )
+        {
+
+
+            var Label = new Literal
+            {
+                FieldWrapper = "div",
+                Wrap = true,
+                DisplayOrder = 10,
+                Html = _FieldTypeID.Attribute("PromptText").Value,
+                Top = _Height * double.Parse(_FieldTypeID.Attribute("ControlTopPositionPercentage").Value),
+                Left = _Width * double.Parse(_FieldTypeID.Attribute("ControlLeftPositionPercentage").Value),
+                CssClass = "EpiLabel",
+                fontSize = double.Parse(_FieldTypeID.Attribute("ControlFontSize").Value),
+                fontfamily = _FieldTypeID.Attribute("ControlFontFamily").Value,
+                fontstyle = _FieldTypeID.Attribute("ControlFontStyle").Value,
+                Height = _Height * double.Parse(_FieldTypeID.Attribute("ControlHeightPercentage").Value),
+                Width = _Width * double.Parse(_FieldTypeID.Attribute("ControlWidthPercentage").Value)
+
+            };
+            return Label;
+
+        }
+    }
+}
---
+++ Epi.Web/Utility/TextAreaProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using MvcDynamicForms.Fields;
+using System.Xml;
+using System.Xml.Linq;
+using System.Text;
+using MvcDynamicForms;
+namespace Epi.Web.MVC.Utility
+{
+    public class TextAreaProvider
+    {
+        public TextArea GetTextArea(XElement _FieldTypeID, double _Width, double _Height, string SurveyAnswer, string _ControlValue)
+        {
+
+
+            var TextArea = new TextArea
+            {
+                Title = "Name",
+                Prompt = _FieldTypeID.Attribute("PromptText").Value,
+                DisplayOrder = 20,
+                Required = true,
+                RequiredMessage = _FieldTypeID.Attribute("PromptText").Value + " is required",
+                Key = _FieldTypeID.Attribute("UniqueId").Value,
+                PromptTop = _Height * double.Parse(_FieldTypeID.Attribute("PromptTopPositionPercentage").Value),
+                PromptLeft = _Width * double.Parse(_FieldTypeID.Attribute("PromptLeftPositionPercentage").Value),
+                Top = _Height * double.Parse(_FieldTypeID.Attribute("ControlTopPositionPercentage").Value),
+                Left = _Width * double.Parse(_FieldTypeID.Attribute("ControlLeftPositionPercentage").Value),
+                PromptWidth = _Width * double.Parse(_FieldTypeID.Attribute("ControlWidthPercentage").Value),
+                ControlWidth = _Width * double.Parse(_FieldTypeID.Attribute("ControlWidthPercentage").Value),
+                fontstyle = _FieldTypeID.Attribute("PromptFontStyle").Value,
+                fontSize = double.Parse(_FieldTypeID.Attribute("PromptFontSize").Value),
+                fontfamily = _FieldTypeID.Attribute("PromptFontFamily").Value,
+                IsRequired = bool.Parse(_FieldTypeID.Attribute("IsRequired").Value),
+                IsReadOnly = bool.Parse(_FieldTypeID.Attribute("IsReadOnly").Value),
+                ControlValue = _ControlValue
+
+
+            };
+            return TextArea;
+
+        }
+    }
+}
