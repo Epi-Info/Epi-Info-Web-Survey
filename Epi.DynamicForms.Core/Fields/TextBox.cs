d6235f7b467aaf787e220f7e1e7fac83e745bc89
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2011-12-28 16:29:51 +0000

    Added changes to Server Validation

--- Epi.DynamicForms.Core/Fields/TextBox.cs
+++ Epi.DynamicForms.Core/Fields/TextBox.cs
@@ -48,7 +48,7 @@ namespace MvcDynamicForms.Fields
             txt.Attributes.Add("id", inputName);
             txt.Attributes.Add("type", "text");
            // txt.Attributes.Add("value", Value);
-            txt.Attributes.Add("value", ControlValue);
+            txt.Attributes.Add("value", Value);
             if(_IsRequired ==true){
             txt.Attributes.Add("class", "validate[required] text-input");
             txt.Attributes.Add("data-prompt-position", "topRight:15");
