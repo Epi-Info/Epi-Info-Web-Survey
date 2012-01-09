4baa39dbbc198319e97920350eaff986855b5f34
icuseven_cp <SND\icuseven_cp>
2012-01-09 19:09:28 +0000

    Updated Download page to download all Survey MetaData and All Survey Resposne Data.

--- Epi.Web.SurveyManager_Test/Page_Download.xaml.cs
+++ Epi.Web.SurveyManager_Test/Page_Download.xaml.cs
@@ -29,5 +29,61 @@ namespace Epi.Web.SurveyManager.Client
             Page_Publish page_Publish = new Page_Publish();
             this.NavigationService.Navigate(page_Publish);
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            SurveyManagerService.ManagerServiceClient client = new SurveyManagerService.ManagerServiceClient();
+
+            Epi.Web.Common.Message.SurveyInfoRequest Request = new Epi.Web.Common.Message.SurveyInfoRequest();
+            
+            if (!string.IsNullOrEmpty(this.SurveyCriteria_SurveyId.Text))
+            {
+                Request.Criteria.SurveyId = this.SurveyCriteria_SurveyId.Text;
+            }
+
+
+            try
+            {
+                Epi.Web.Common.Message.SurveyInfoResponse Result = client.GetSurveyInfo(Request);
+                
+                foreach(Epi.Web.Common.DTO.SurveyInfoDTO SurveyInfo in Result.SurveyInfo)
+                {
+                    SurveyInfoResponseTextBox.AppendText(SurveyInfo.SurveyId + " " + SurveyInfo.SurveyName + "\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                SurveyInfoResponseTextBox.AppendText("error:\n");
+                SurveyInfoResponseTextBox.AppendText(ex.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            SurveyManagerService.ManagerServiceClient client = new SurveyManagerService.ManagerServiceClient();
+
+            Epi.Web.Common.Message.SurveyAnswerRequest Request = new Epi.Web.Common.Message.SurveyAnswerRequest();
+
+            if (!string.IsNullOrEmpty(this.SurveyCriteria_SurveyId.Text))
+            {
+                Request.Criteria.SurveyId = this.SurveyCriteria_SurveyId.Text;
+            }
+
+
+            try
+            {
+                Epi.Web.Common.Message.SurveyAnswerResponse Result = client.GetSurveyAnswer(Request);
+
+                foreach (Epi.Web.Common.DTO.SurveyAnswerDTO SurveyAnswer in Result.SurveyResponseList)
+                {
+                    SurveyAnswerResponseTextBox.AppendText(string.Format("{0} - {1} - {2}\n",SurveyAnswer.SurveyId, SurveyAnswer.Status, SurveyAnswer.DateLastUpdated));
+                }
+            }
+            catch (Exception ex)
+            {
+                SurveyAnswerResponseTextBox.AppendText("error:\n");
+                SurveyAnswerResponseTextBox.AppendText(ex.ToString());
+            }
+        }
     }
 }
