032cd8f19be57a5d34538330b1d4af2ecb1649ae
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2012-02-01 15:29:34 +0000

    Code to handle multiple pages and navigation between them (response XML)

--- Epi.Web/Utility/SurveyHelper.cs
+++ Epi.Web/Utility/SurveyHelper.cs
@@ -5,7 +5,10 @@ using Epi.Web.MVC.Constants;
 using Epi.Web.MVC.Utility;
 using Epi.Web.MVC.Models;
 using Epi.Web.MVC.Facade;
-
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+using System.Xml.XPath;
 namespace Epi.Web.MVC.Utility
 {
     public class SurveyHelper
@@ -23,12 +26,13 @@ namespace Epi.Web.MVC.Utility
                                           Common.DTO.SurveyAnswerDTO surveyAnswerDTO,
                                           SurveyResponseXML surveyResponseXML, ISurveyAnswerRepository iSurveyAnswerRepository)
         {
+            bool AddRoot = false;
             surveyAnswerRequest.Criteria.SurveyAnswerIdList.Add(responseId.ToString());
             surveyAnswerDTO.ResponseId = responseId.ToString();
             surveyAnswerDTO.DateCompleted = DateTime.Now;
             surveyAnswerDTO.SurveyId = surveyId;
             surveyAnswerDTO.Status = (int)Constant.Status.InProgress;
-            surveyAnswerDTO.XML = surveyResponseXML.CreateResponseXml(surveyId).InnerXml;
+            surveyAnswerDTO.XML = surveyResponseXML.CreateResponseXml(surveyId, AddRoot,0).InnerXml;
             surveyAnswerRequest.SurveyAnswerList.Add(surveyAnswerDTO);
             surveyAnswerRequest.Action = Epi.Web.MVC.Constants.Constant.CREATE;  //"Create";
             iSurveyAnswerRepository.SaveSurveyAnswer(surveyAnswerRequest);
@@ -37,7 +41,7 @@ namespace Epi.Web.MVC.Utility
         public static void UpdateSurveyResponse(SurveyInfoModel surveyInfoModel,MvcDynamicForms.Form form, SurveyAnswerRequest surveyAnswerRequest,
                                                              SurveyResponseXML surveyResponseXML,
                                                             ISurveyAnswerRepository iSurveyAnswerRepository,
-                                                             SurveyAnswerResponse surveyAnswerResponse,string responseId)
+                                                             SurveyAnswerResponse surveyAnswerResponse,string responseId, Epi.Web.Common.DTO.SurveyAnswerDTO surveyAnswerDTO)
         {
             // 1 Get the record for the current survey response
             // 2 update the current survey response
@@ -47,10 +51,25 @@ namespace Epi.Web.MVC.Utility
             // 2 a. update the current survey answer request
             surveyAnswerRequest.SurveyAnswerList = surveyAnswerResponse.SurveyResponseList;
             surveyResponseXML.Add(form);
-            surveyAnswerRequest.SurveyAnswerList[0].XML = surveyResponseXML.CreateResponseXml(surveyInfoModel.SurveyId).InnerXml;
+            XDocument SavedXml = XDocument.Parse(surveyAnswerDTO.XML);
+            bool AddRoot = false;
+            if (SavedXml.Root.FirstAttribute.Value.ToString() == "0")
+            {
+                  AddRoot = true;
+            }
+            surveyAnswerRequest.SurveyAnswerList[0].XML = surveyResponseXML.CreateResponseXml(surveyInfoModel.SurveyId,AddRoot,form.CurrentPage).InnerXml;
             // 2 b. save the current survey response
             surveyAnswerRequest.Action = Epi.Web.MVC.Constants.Constant.UPDATE;  //"Update";
+            //Append to Response Xml
+           
+            XDocument CurrentPageResponseXml = XDocument.Parse(surveyAnswerRequest.SurveyAnswerList[0].XML);
+            if (SavedXml.Root.FirstAttribute.Value.ToString() != "0")
+            { 
+                surveyAnswerRequest.SurveyAnswerList[0].XML = MergeXml(SavedXml, CurrentPageResponseXml,form.CurrentPage).ToString(); 
+            }
+ 
             iSurveyAnswerRepository.SaveSurveyAnswer(surveyAnswerRequest);
+           
         }
 
 
@@ -71,5 +90,29 @@ namespace Epi.Web.MVC.Utility
             surveyInfoRequest.Criteria.SurveyIdList.Add(SurveyId);
             return iSurveyInfoRepository.GetSurveyInfo(surveyInfoRequest).SurveyInfoList[0];
         }
+
+        public static XDocument MergeXml(XDocument SavedXml, XDocument CurrentPageResponseXml, int Pagenumber)
+        {
+
+            XDocument xdoc = XDocument.Parse(SavedXml.ToString());
+            XElement oldXElement = xdoc.XPathSelectElement("SurveyResponse/Page[@PageNumber = '" + Pagenumber.ToString() + "']");
+ 
+
+            if (oldXElement == null)
+            {
+                SavedXml.Root.Add(CurrentPageResponseXml.Elements());
+                return SavedXml;
+            }
+
+            else 
+            {
+                oldXElement.Remove();
+                xdoc.Root.Add(CurrentPageResponseXml.Elements());
+                return xdoc;
+            }
+            
+        
+        }
+
     }
 }
