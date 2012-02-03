f7d63d5c9785cb22d5548c2cf7a5184160ea9337
mlamtahri_cp <SND\mlamtahri_cp>
2012-02-03 18:05:16 +0000

    Code to handle the scenario when a Survey participant reloads a saved survey

--- Epi Info Web Survey/Epi.Web/Controllers/HomeController.cs
+++ Epi Info Web Survey/Epi.Web/Controllers/HomeController.cs
@@ -40,7 +40,15 @@ namespace Epi.Web.MVC.Controllers
              if surveyInfodto.SurveyName== null then go to the exception page*/
             try
             {
+                //create the responseid
+                Guid ResponseID = Guid.NewGuid();
 
+                //put the ResponseId in Temp data for later use
+                TempData[Epi.Web.MVC.Constants.Constant.RESPONSE_ID] = ResponseID.ToString();
+
+                // create the first survey response
+                _isurveyFacade.CreateSurveyAnswer(surveyid, ResponseID.ToString());
+              //  TempData[Epi.Web.MVC.Constants.Constant.RESPONSE_ID] = "";
                 SurveyInfoModel surveyInfoModel = _isurveyFacade.GetSurveyInfoModel(surveyid);
                 return View(Epi.Web.MVC.Constants.Constant.INDEX_PAGE, surveyInfoModel);
             }
@@ -49,6 +57,30 @@ namespace Epi.Web.MVC.Controllers
                 return View(Epi.Web.MVC.Constants.Constant.EXCEPTION_PAGE);
             }
         }
+
+
+
+        public ActionResult Response(string responseid, int StatusId)
+        {
+            TempData[Epi.Web.MVC.Constants.Constant.RESPONSE_ID] = responseid;
+             
+            try
+            {
+                SurveyInfoModel surveyInfoModel = _isurveyFacade.GetSurveyInfoModel(GetCurrentSurveyAnswer().SurveyId.ToString());
+                if (surveyInfoModel.ClosingDate > DateTime.Now && StatusId == GetCurrentSurveyAnswer().Status)
+                {
+                return RedirectToRoute(new { Controller = "Survey", Action = "Index", surveyId = GetCurrentSurveyAnswer().SurveyId.ToString(), PageNumber = 1 });
+                }
+                else{
+                 return View(Epi.Web.MVC.Constants.Constant.EXCEPTION_PAGE);
+                }
+            }
+            catch (Exception ex)
+            {
+                return View(Epi.Web.MVC.Constants.Constant.EXCEPTION_PAGE);
+            }
+        }
+
 
         /// <summary>
         /// redirecting to Survey controller to action method Index
@@ -70,6 +102,27 @@ namespace Epi.Web.MVC.Controllers
             }
          }
 
+        private Epi.Web.Common.DTO.SurveyAnswerDTO GetCurrentSurveyAnswer()
+        {
+            Epi.Web.Common.DTO.SurveyAnswerDTO result = null;
 
+            if (TempData.ContainsKey(Epi.Web.MVC.Constants.Constant.RESPONSE_ID) && TempData[Epi.Web.MVC.Constants.Constant.RESPONSE_ID] != null
+                                                                               && !string.IsNullOrEmpty(TempData[Epi.Web.MVC.Constants.Constant.RESPONSE_ID].ToString()))
+            {
+
+                string responseId = TempData[Epi.Web.MVC.Constants.Constant.RESPONSE_ID].ToString();
+
+
+                //TODO: Now repopulating the TempData (by reassigning to responseId) so it persisits, later we will need to find a better 
+                //way to replace it. 
+                TempData[Epi.Web.MVC.Constants.Constant.RESPONSE_ID] = responseId;
+                return _isurveyFacade.GetSurveyAnswerResponse(responseId).SurveyResponseList[0];
+
+
+
+            }
+
+            return result;
+        }
     }
 }
