79323eaea0b844c75127f927924af0023a3b284d
TIsmail_cp <SND\TIsmail_cp>
2012-02-08 18:48:40 +0000

    email functionality implemented and tested

--- Epi Info Web Survey/Epi.Web/Utility/EmailMessage.cs
+++ Epi Info Web Survey/Epi.Web/Utility/EmailMessage.cs
@@ -19,12 +19,27 @@ namespace Epi.Web.Utility
         {
             try
             {
+                //System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
+                //message.To.Add(emailAddress);
+                //message.Subject = ConfigurationManager.AppSettings["EMAIL_SUBJECT"].ToString();   //"This is the Subject line";
+                //message.From = new System.Net.Mail.MailAddress(ConfigurationManager.AppSettings["EMAIL_FROM"].ToString());//sender email address
+                //message.Body = redirectUrl;
+                //System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient(ConfigurationManager.AppSettings["PORT"].ToString());//port 
+                //smtp.Send(message);
+                //return true;
+
+                //for test
                 System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
                 message.To.Add(emailAddress);
                 message.Subject = ConfigurationManager.AppSettings["EMAIL_SUBJECT"].ToString();   //"This is the Subject line";
                 message.From = new System.Net.Mail.MailAddress(ConfigurationManager.AppSettings["EMAIL_FROM"].ToString());//sender email address
                 message.Body = redirectUrl;
-                System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient(ConfigurationManager.AppSettings["PORT"].ToString());//port 
+                System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient(ConfigurationManager.AppSettings["SMTP_HOST"].ToString());//server 
+                smtp.Credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["EMAIL_FROM"].ToString(), ConfigurationManager.AppSettings["EMAIL_PASSWORD"].ToString());
+                smtp.EnableSsl = true;
+                smtp.Port = Convert.ToInt16(ConfigurationManager.AppSettings["SMTP_PORT"].ToString());
+                smtp.EnableSsl = true;
+
                 smtp.Send(message);
                 return true;
 
