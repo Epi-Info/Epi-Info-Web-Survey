84ce8544dbcd99630e4649194c7b4e7ea32e753e
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2012-02-21 16:46:20 +0000

    Added .htc file

--- Epi.DynamicForms.Core/Fields/TextBox.cs
+++ Epi.DynamicForms.Core/Fields/TextBox.cs
@@ -42,6 +42,10 @@ namespace MvcDynamicForms.Fields
             txt.Attributes.Add("id", inputName);
             txt.Attributes.Add("type", "text");
            // txt.Attributes.Add("value", Value);
+            ////////////Check code start//////////////////
+            txt.Attributes.Add("onfocus", "EventArray.push('" + Prompt + "before')");//befor
+            txt.Attributes.Add("onblur", "EventArray.push('" + Prompt + "After')");//After
+            ////////////Check code end//////////////////
             txt.Attributes.Add("value", Value);
             if(_IsRequired ==true){
             txt.Attributes.Add("class", "validate[required] text-input");
