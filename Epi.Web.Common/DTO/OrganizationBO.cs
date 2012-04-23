4fdf8bdf95a1c261b99cca104cd1bc50ed5d6cc7
icuseven_cp <SND\icuseven_cp>
2012-04-23 17:45:29 +0000

    Added OrganizationBO, OrganizationDTO and OrganizationCriteria objects to support Organization Plublish Access

---
+++ Epi.Web.Common/DTO/OrganizationBO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Epi.Web.Common.BusinessObject
+{
+    public class OrganizationBO
+    {
+        private string _Organization;
+        private Guid _OrganizationKey;
+        private bool _IsEnabled;
+
+
+        public string Organization
+        {
+            get { return _Organization; }
+            set { _Organization = value; }
+        }
+
+        public Guid OrganizationKey 
+        {
+            get { return _OrganizationKey; }
+            set { _OrganizationKey = value; }
+        }
+
+        public bool IsEnabled
+        {
+            get { return _IsEnabled; }
+            set { _IsEnabled = value; }
+        }
+
+
+    }
+}
