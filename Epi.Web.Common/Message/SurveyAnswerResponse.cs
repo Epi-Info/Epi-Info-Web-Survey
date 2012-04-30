b921f81af6579515326d3abab35fb7c914f88da7
TIsmail_cp <SND\TIsmail_cp>
2012-04-30 19:29:55 +0000

    Adding PageInfoDTO and PageInfoBO

--- Epi.Web.Common/Message/SurveyAnswerResponse.cs
+++ Epi.Web.Common/Message/SurveyAnswerResponse.cs
@@ -33,7 +33,7 @@ namespace Epi.Web.Common.Message
         /// Total number of pages for query
         /// </summary>
         [DataMember]
-        public int PageTotal { get; set; }
+        public int NumberOfPages { get; set; }
 
         /// <summary>
         /// Number of Records per page
