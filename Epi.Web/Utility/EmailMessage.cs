f3d6f6d00e5b5703537c2526939a3631222e540e
icuseven_cp <SND\icuseven_cp>
2012-07-12 13:51:09 +0000

    Refactored Email App Config Settings to support:                 // App Config Settings:                 // EMAIL_USE_AUTHENTICATION [ True | False ] default is False                 // EMAIL_USE_SSL [ True | False] default is False                 // SMTP_HOST [ url or ip address of smtp server ]                 // SMTP_PORT [ port number to use ] default is 25                 // EMAIL_FROM [ email address of sender and authenticator ]                 // EMAIL_PASSWORD [ password of sender and authenticator ]
    Refactored SendMessage to support configuration of Use_Authentication and  Use_SSL settings.

--- Epi.Web/Utility/EmailMessage.cs
+++ Epi.Web/Utility/EmailMessage.cs
@@ -9,65 +9,73 @@ namespace Epi.Web.Utility
 {
     public class EmailMessage
     {
-        /// <summary>
-        /// the following method takes email and responseUrl as argument and email redirection url to the user 
-        /// </summary>
-        /// <param name="email"></param>
-        /// <param name="response"></param>
-        /// <returns></returns>
-        public static bool SendMessage_VerifiedGoogle(string emailAddress, string redirectUrl,string surveyName,string passCode)
-        {
-            try
-            {
-                //System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
-                //message.To.Add(emailAddress);
-                //message.Subject = ConfigurationManager.AppSettings["EMAIL_SUBJECT"].ToString();   //"This is the Subject line";
-                //message.From = new System.Net.Mail.MailAddress(ConfigurationManager.AppSettings["EMAIL_FROM"].ToString());//sender email address
-                //message.Body = redirectUrl;
-                //System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient(ConfigurationManager.AppSettings["PORT"].ToString());//port 
-                //smtp.Send(message);
-                //return true;
-
-                //for test
-                System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
-                message.To.Add(emailAddress);
-                message.Subject = "Link for Survey: " + surveyName; //ConfigurationManager.AppSettings["EMAIL_SUBJECT"].ToString();   //"This is the Subject line";
-                message.From = new System.Net.Mail.MailAddress(ConfigurationManager.AppSettings["EMAIL_FROM"].ToString());//sender email address
-                message.Body = redirectUrl + " and Pass Code is: " + passCode;
-                
-                System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient(ConfigurationManager.AppSettings["SMTP_HOST"].ToString());//server 
-                smtp.Credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["EMAIL_FROM"].ToString(), ConfigurationManager.AppSettings["EMAIL_PASSWORD"].ToString());
-                //smtp.EnableSsl = true;
-                smtp.Port = Convert.ToInt16(ConfigurationManager.AppSettings["SMTP_PORT"].ToString());
-                smtp.EnableSsl = true;
-
-                smtp.Send(message);
-                return true;
-
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
-        }
 
         /// <summary>
         /// the following method takes email and responseUrl as argument and email redirection url to the user 
         /// </summary>
-        /// <param name="email"></param>
-        /// <param name="response"></param>
+        /// <param name="emailAddress">email address for sending message (email is NOT saved)</param>
+        /// <param name="redirectUrl">url for resuming the saved survey</param>
+        /// <param name="surveyName">Name of the survey</param>
+        /// <param name="passCode"> Code for accessing an unfinished survey </param>
         /// <returns></returns>
         public static bool SendMessage(string emailAddress, string redirectUrl, string surveyName, string passCode)
         {
             try
             {
+                bool isAuthenticated = false;
+                bool isUsingSSL = false;
+                int SMTPPort = 25;
+
+                // App Config Settings:
+                // EMAIL_USE_AUTHENTICATION [ True | False ] default is False
+                // EMAIL_USE_SSL [ True | False] default is False
+                // SMTP_HOST [ url or ip address of smtp server ]
+                // SMTP_PORT [ port number to use ] default is 25
+                // EMAIL_FROM [ email address of sender and authenticator ]
+                // EMAIL_PASSWORD [ password of sender and authenticator ]
+
+
+                string s = ConfigurationManager.AppSettings["EMAIL_USE_AUTHENTICATION"];
+                if (!String.IsNullOrEmpty(s))
+                {
+                    if (s.ToUpper() == "TRUE")
+                    {
+                        isUsingSSL = true;
+                    }
+                }
+
+                s = ConfigurationManager.AppSettings["EMAIL_USE_SSL"];
+                if (!String.IsNullOrEmpty(s))
+                {
+                    if (s.ToUpper() == "TRUE")
+                    {
+                        isUsingSSL = true;
+                    }
+                }
+
+                s = ConfigurationManager.AppSettings["SMTP_PORT"];
+                if (!int.TryParse(s, out SMTPPort))
+                {
+                    SMTPPort = 25;
+                }
+
                 System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
                 message.To.Add(emailAddress);
                 message.Subject = "Link for Survey: " + surveyName; 
                 message.From = new System.Net.Mail.MailAddress(ConfigurationManager.AppSettings["EMAIL_FROM"].ToString());
                 message.Body = redirectUrl + " and Pass Code is: " + passCode;
-
                 System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient(ConfigurationManager.AppSettings["SMTP_HOST"].ToString());
+                smtp.Port = SMTPPort;
+
+                if (isAuthenticated)
+                {
+                    smtp.Credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["EMAIL_FROM"].ToString(), ConfigurationManager.AppSettings["EMAIL_PASSWORD"].ToString());
+                }
+
+                if (isUsingSSL)
+                {
+                    smtp.EnableSsl = true;
+                }
 
                 smtp.Send(message);
 
