6b4452b020080e0d3ee9516c3a93008af32c8d0a
icuseven_cp <SND\icuseven_cp>
2012-08-15 14:51:37 +0000

    Dialog Check-in

--- Epi.Web.CheckCodeEngine/Rules/Functions/Rule_FindText.cs
+++ Epi.Web.CheckCodeEngine/Rules/Functions/Rule_FindText.cs
@@ -36,5 +36,15 @@ namespace Epi.Core.EnterInterpreter.Rules
 
             return result;
         }
+
+
+        public override void ToJavaScript(StringBuilder pJavaScriptBuilder)
+        {
+            pJavaScriptBuilder.Append("CCE_FindText(");
+            this.ParameterList[0].ToJavaScript(pJavaScriptBuilder);
+            pJavaScriptBuilder.Append(",");
+            this.ParameterList[1].ToJavaScript(pJavaScriptBuilder);
+            pJavaScriptBuilder.Append(")");
+        }
     }
 }
