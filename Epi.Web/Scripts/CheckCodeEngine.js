8ede69f1a5c2aa0b9ac88e13609b3ea3d7fcc78d
icuseven_cp <SND\icuseven_cp>
2012-10-12 13:48:47 +0000

    Updated Numeric Text Box ServerSide Validation. Updated SetTime for TimePicker Control.

--- Epi.Web/Scripts/CheckCodeEngine.js
+++ Epi.Web/Scripts/CheckCodeEngine.js
@@ -370,9 +370,9 @@ CCE_Context.prototype.setValue = function (pName, pValue)
                         cce_Symbol.Value = pValue;
                         break;
                     case "timepicker":
-                        //$(Jquery).timepicker("setTime", new Date(pValue));
-                        //cce_Symbol.Value = pValue;
-                        //break;
+                        $(Jquery).timepicker("setTime", new Date(pValue));
+                        cce_Symbol.Value = pValue;
+                        break;
                    default:
                         $(Jquery).val(pValue);
                         cce_Symbol.Value = pValue;
@@ -1101,44 +1101,7 @@ function CCE_SystemDate()
 
 function CCE_SystemTime()
 {
-    //return new Date().getTime();
-var PMAM ="";
-var FormatedTime = "";
-var currentTime = new Date();
-var hours = currentTime.getHours();
-var minutes = currentTime.getMinutes();
-var seconds = currentTime.getSeconds();
-    if (minutes < 10)
-    {
-    minutes = "0" + minutes
-    }
-    if (seconds < 10)
-    {
-    seconds = "0" + seconds
-    }
-     
-    
-    if ( hours < 10 )  
-    {
-       hours = "0" + hours
-    }
-    if(hours > 11)
-    {
-       PMAM = "PM";
-    } else {
-       PMAM = "AM" ;
-    }
-    if (hours > 12) 
-    {
-        hours = hours - 12; 
-        if ( hours < 10 )  
-        {
-           hours = "0" + hours
-        } 
-    }
-    FormatedTime = hours + ":" + minutes + ":" + seconds + " " + PMAM;
-    return FormatedTime;
-
+    return new Date().getTime();
 }
 
 
