77d8793d6fd76ffe2692a28bd0434721f4e9a0fa
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2012-12-19 15:56:11 +0000

    Added read only property to date picker and time picker.

--- Epi.DynamicForms.Core/Fields/DatePicker.cs
+++ Epi.DynamicForms.Core/Fields/DatePicker.cs
@@ -81,6 +81,13 @@ namespace MvcDynamicForms.Fields
             txt.MergeAttributes(_inputHtmlAttributes);
             html.Append(txt.ToString(TagRenderMode.SelfClosing));
 
+            // If readonly then add the following jquery script to make the field disabled 
+            if (ReadOnly)
+            {
+                var scriptReadOnlyText = new TagBuilder("script");
+                scriptReadOnlyText.InnerHtml = "$(function(){$('#" + inputName + "').attr('disabled','disabled')});";
+                html.Append(scriptReadOnlyText.ToString(TagRenderMode.Normal));
+            }
             // adding scripts for date picker
             var scriptDatePicker = new TagBuilder("script");
             //scriptDatePicker.InnerHtml = "$(function() { $('#" + inputName + "').datepicker({changeMonth: true,changeYear: true});});";
