e019d5d799a5c1a149f7ffeaf5e7b8e64cc2db8f
icuseven_cp <SND\icuseven_cp>
2013-01-14 19:12:36 +0000

    Updated Autosearch to support new displaylist option. Updated Unhide to support wildcard without the except option.

--- Epi.Web.CheckCodeEngine/Rules/Rule_AutoSearch.cs
+++ Epi.Web.CheckCodeEngine/Rules/Rule_AutoSearch.cs
@@ -8,11 +8,37 @@ namespace Epi.Core.EnterInterpreter.Rules
     public class Rule_AutoSearch: EnterRule 
     {
         string[] IdentifierList = null;
+        bool AlwaysShow = false;
+        string[] DisplayList = null;
 
-        public Rule_AutoSearch(Rule_Context pContext, NonterminalToken pToken) : base(pContext)
+        public Rule_AutoSearch(Rule_Context pContext, NonterminalToken pToken)
+            : base(pContext)
         {
-            //<Auto_Search_Statement> ::= AUTOSEARCH <IdentifierList> 
+            /*<Auto_Search_Statement> ::= AUTOSEARCH <IdentifierList> 
+                                          |AUTOSEARCH <IdentifierList> Always
+                                          |AUTOSEARCH <IdentifierList> DisplayList <IdentifierList> 
+                                          |AUTOSEARCH <IdentifierList> DisplayList <IdentifierList> Always*/
+
+
             this.IdentifierList = this.GetCommandElement(pToken.Tokens, 1).Split(' ');
+            if (pToken.Tokens.Length > 2)
+            {
+
+                if (pToken.Tokens[2].ToString().Equals("Always", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.AlwaysShow = true;
+                }
+                else
+                {
+                    this.DisplayList = this.GetCommandElement(pToken.Tokens, 2).Split(' ');
+                }
+            }
+
+
+            if (pToken.Tokens.Length == 5)
+            {
+                this.AlwaysShow = true;
+            }
         }
 
 
@@ -22,7 +48,7 @@ namespace Epi.Core.EnterInterpreter.Rules
         /// <returns>object</returns>
         public override object Execute()
         {
-            this.Context.EnterCheckCodeInterface.AutoSearch(this.IdentifierList);
+            this.Context.EnterCheckCodeInterface.AutoSearch(this.IdentifierList);//, this.DisplayList, this.AlwaysShow);
             return null;
         }
     }
