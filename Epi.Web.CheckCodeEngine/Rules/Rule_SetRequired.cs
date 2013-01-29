2c2f6f8193498517654d94df8245de5ed3e5f1c3
icuseven_cp <SND\icuseven_cp>
2013-01-29 16:42:22 +0000

    Added basic Set-Required and Set-Not-Required Rules.

---
+++ Epi.Web.CheckCodeEngine/Rules/Rule_SetRequired.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using com.calitha.goldparser;
+
+namespace Epi.Core.EnterInterpreter.Rules
+{
+    public class Rule_SetRequired : EnterRule
+    {
+        string[] IdentifierList = null;
+
+        public Rule_SetRequired(Rule_Context pContext, NonterminalToken pToken)
+        {
+            this.IdentifierList = this.IdentifierList = this.GetCommandElement(pToken.Tokens, 1).ToString().Split(' '); 
+        }
+
+        /// <summary>
+        /// Executes the reduction
+        /// </summary>
+        /// <returns>null</returns>
+        public override object Execute()
+        {
+            this.Context.EnterCheckCodeInterface.SetRequired(this.IdentifierList);
+            return null;
+        }
+    }
+}
