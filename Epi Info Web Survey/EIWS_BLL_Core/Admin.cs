e4bef07e0b01577bb0f3c6437fec75f7db29db61
mlamtahri_cp <SND\mlamtahri_cp>
2013-08-02 15:35:58 +0000

    Epi Info Web Survey: Manager Service code refactoring

--- Epi Info Web Survey/EIWS_BLL_Core/Admin.cs
+++ Epi Info Web Survey/EIWS_BLL_Core/Admin.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Epi.Web.Common.BusinessObject;
 using Epi.Web.Common.Criteria;
 using System.Configuration;
+using Epi.Web.Common.Exception;
 namespace Epi.Web.BLL
     {
    public class Admin
@@ -29,6 +30,55 @@ namespace Epi.Web.BLL
                {
                List<AdminBO> result = this.AdminDao.GetAdminInfoByOrgId(OrgId);
                return result;
+               }
+          
+           private List<AdminBO> GetOrganizationAdmins(SurveyInfoBO request)
+               {
+              
+              
+               List<AdminBO> AdminBOList = new List<AdminBO>(); 
+               try
+                   {
+
+                   Epi.Web.Interfaces.DataInterfaces.IAdminDao AdminDao = new EF.EntityAdminDao();
+                   Epi.Web.BLL.Admin Implementation = new Epi.Web.BLL.Admin(AdminDao);
+                   AdminBOList = Implementation.GetAdminInfoByOrgKey(request.OrganizationKey.ToString());
+                  
+                 
+                }
+               catch (Exception ex)
+                   {
+                   CustomFaultException customFaultException = new CustomFaultException();
+                   customFaultException.CustomMessage = ex.Message;
+                   customFaultException.Source = ex.Source;
+                   customFaultException.StackTrace = ex.StackTrace;
+                   customFaultException.HelpLink = ex.HelpLink;
+                    
+                 }
+               return AdminBOList;
+               }
+
+           public void SendEmailToAdmins(SurveyInfoBO SurveyInfo)
+               {
+               Epi.Web.Common.Message.AdminRequest AdminRequest = new Web.Common.Message.AdminRequest();
+               Epi.Web.Common.Message.AdminResponse AdminResult = new Web.Common.Message.AdminResponse();
+               AdminRequest.OrganizationKey = new Guid(SurveyInfo.OrganizationKey.ToString()).ToString();
+                List<AdminBO> AdminBOList = new  List<AdminBO>();
+               List<string> AdminList = new List<string>();
+              
+               AdminBOList = GetOrganizationAdmins(SurveyInfo);
+               foreach (var item in AdminBOList)
+                   {
+                   AdminList.Add(item.AdminEmail);
+                   }
+
+               Epi.Web.Common.Email.Email Email = new Web.Common.Email.Email();
+               Email.Body = "The following survey has been promoted to FINAL mode:\n Title:" + SurveyInfo.SurveyName + " \n Survey ID:" + SurveyInfo.SurveyId + " \nOrganization:" + SurveyInfo.OrganizationName + "\n Start Date & Time:" + SurveyInfo.StartDate + "\n Closing Date & Time:" + SurveyInfo.ClosingDate + " \n \n \n  Thank you.";
+               Email.From = ConfigurationManager.AppSettings["EMAIL_FROM"];
+               Email.To = AdminList;
+               Email.Subject = "Survey -" + SurveyInfo.SurveyName + " has been promoted to FINAL";
+               bool success = Epi.Web.Common.Email.EmailHandler.SendMessage(Email);
+
                }
         }
     }
