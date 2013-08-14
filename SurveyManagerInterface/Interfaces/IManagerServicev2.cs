5bf216c2c2579a83a8882624c83b68a49e2d3caa
Sachin Agnihotri <12765535+sax5@users.noreply.github.com>
2013-08-14 19:44:25 +0000

    Epi.Web.WCF.SurveyService: Creation of ManagerService version 2 interface and implemenation class

---
+++ SurveyManagerInterface/Interfaces/IManagerServicev2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Epi.Web.Common.DTO;
+using Epi.Web.Common.Message;
+using Epi.Web.Common.Exception;
+
+namespace Epi.Web.WCF.SurveyService
+{
+    [ServiceContract]
+    public interface IManagerServicev2
+    {
+        [OperationContract]
+        [FaultContract(typeof(CustomFaultException))]
+        PublishResponse RePublishSurvey(PublishRequest pRequestMessage);
+    }
+}
