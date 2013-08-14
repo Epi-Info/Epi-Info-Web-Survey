3fe6d06fb4548c0df8c5fdb50ab5b8634d8933ae
Sachin Agnihotri <12765535+sax5@users.noreply.github.com>
2013-08-14 19:44:25 +0000

    Epi.Web.WCF.SurveyService: Creation of ManagerService version 2 interface and implemenation class

--- SurveyManagerInterface/Interfaces/IManagerService.cs
+++ SurveyManagerInterface/Interfaces/IManagerService.cs
@@ -18,10 +18,6 @@ namespace Epi.Web.WCF.SurveyService
 
         [OperationContract]
         [FaultContract(typeof(CustomFaultException))]
-        PublishResponse RePublishSurvey(PublishRequest pRequestMessage);
-
-        [OperationContract]
-        [FaultContract(typeof(CustomFaultException))]
         SurveyInfoResponse GetSurveyInfo(SurveyInfoRequest pRequest);
 
         [OperationContract]
---
+++ SurveyManagerInterface/ManagerServicev2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Epi.Web.Common.DTO;
+using Epi.Web.Common.Message;
+using Epi.Web.Common.MessageBase;
+using Epi.Web.Common.Criteria;
+using Epi.Web.Common.ObjectMapping;
+using Epi.Web.Common.BusinessObject;
+using Epi.Web.Common.Exception;
+using Epi.Web.BLL;
+
+using System.Configuration;
+using Epi.Web.Common.Security;
+
+namespace Epi.Web.WCF.SurveyService
+{
+    class ManagerServicev2: ManagerService, IManagerServicev2
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pRequestMessage"></param>
+        /// <returns></returns>
+        public PublishResponse RePublishSurvey(PublishRequest pRequest)
+        {
+            try
+            {
+                PublishResponse result = new PublishResponse(pRequest.RequestId);
+                Epi.Web.Interfaces.DataInterfaces.ISurveyInfoDao SurveyInfoDao = new EF.EntitySurveyInfoDao();
+                Epi.Web.Interfaces.DataInterfaces.IOrganizationDao OrganizationDao = new EF.EntityOrganizationDao();
+
+
+                Epi.Web.BLL.Publisher Implementation = new Epi.Web.BLL.Publisher(SurveyInfoDao, OrganizationDao);
+                SurveyInfoBO surveyInfoBO = Mapper.ToBusinessObject(pRequest.SurveyInfo);
+                SurveyRequestResultBO surveyRequestResultBO = Implementation.RePublishSurvey(surveyInfoBO);
+                result.PublishInfo = Mapper.ToDataTransferObject(surveyRequestResultBO);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                CustomFaultException customFaultException = new CustomFaultException();
+                customFaultException.CustomMessage = ex.Message;
+                customFaultException.Source = ex.Source;
+                customFaultException.StackTrace = ex.StackTrace;
+                customFaultException.HelpLink = ex.HelpLink;
+                throw new FaultException<CustomFaultException>(customFaultException);
+            }
+        }
+    }
+}
