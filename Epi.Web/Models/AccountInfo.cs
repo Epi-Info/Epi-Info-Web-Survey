3f44e2526d0625db01a6cbf8331821dc91594200
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2013-10-01 15:52:55 +0000

    Epi Info Web Survey:Account creation code refactoring.

---
+++ Epi.Web/Models/AccountInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+ 
+using System.Web.Mvc;
+namespace Epi.Web.MVC.Models
+    {
+    public class AccountInfo
+        {
+        private string _Email;
+        private string _ConfirmEmail;
+        private string _OrgName;
+        private string _Status;
+         [Required(ErrorMessage = "Email is required.")]
+         [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Invalid email address.")]
+        public string Email
+            {
+            get { return _Email; }
+            set { _Email = value; }
+            }
+          [Required(ErrorMessage = "The organization name is required.")]
+        public string OrgName
+            {
+            get { return _OrgName; }
+            set { _OrgName = value; }
+            }
+          [Required(ErrorMessage = "Confirm email is required.")]
+          [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Invalid email address.")]
+          [Compare("Email", ErrorMessage = "The email and confirmation do not match.")]
+          public string ConfirmEmail
+              {
+              get { return _ConfirmEmail; }
+              set { _ConfirmEmail = value; }
+              }
+          public string Status
+              {
+              get { return _Status; }
+              set { _Status = value; }
+              }
+        }
+    }
