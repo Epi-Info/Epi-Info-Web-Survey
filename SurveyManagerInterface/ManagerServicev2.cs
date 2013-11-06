9edaa5a01971b25001ee5cbebcb38b033fb71c90
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2013-11-06 15:06:57 +0000

    Epi Info Web Survey: Fixed Service reference

--- SurveyManagerInterface/ManagerServicev2.cs
+++ SurveyManagerInterface/ManagerServicev2.cs
@@ -12,44 +12,90 @@ using Epi.Web.Common.ObjectMapping;
 using Epi.Web.Common.BusinessObject;
 using Epi.Web.Common.Exception;
 using Epi.Web.BLL;
-
+using System.Xml;
+using System.Xml.Linq;
 using System.Configuration;
 using Epi.Web.Common.Security;
-
 namespace Epi.Web.WCF.SurveyService
-{
-    class ManagerServicev2: ManagerService, IManagerServicev2
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="pRequestMessage"></param>
-        /// <returns></returns>
-        public PublishResponse RePublishSurvey(PublishRequest pRequest)
+    public class ManagerServiceV2 : ManagerService, IManagerServiceV2
         {
+        public PreFilledAnswerResponse SetSurveyAnswer(PreFilledAnswerRequest request)
+            {
             try
-            {
-                PublishResponse result = new PublishResponse(pRequest.RequestId);
-                Epi.Web.Interfaces.DataInterfaces.ISurveyInfoDao SurveyInfoDao = new EF.EntitySurveyInfoDao();
-                Epi.Web.Interfaces.DataInterfaces.IOrganizationDao OrganizationDao = new EF.EntityOrganizationDao();
+                {
+                Dictionary<string, string> ErrorMessageList = new Dictionary<string, string>();
+                PreFilledAnswerResponse response;
+                Epi.Web.Interfaces.DataInterfaces.ISurveyResponseDao SurveyResponseDao = new EF.EntitySurveyResponseDao();
+                Epi.Web.BLL.SurveyResponse Implementation = new Epi.Web.BLL.SurveyResponse(SurveyResponseDao);
 
+                SurveyResponseBO SurveyResponse = new SurveyResponseBO();
+                UserAuthenticationRequestBO UserAuthenticationRequestBO = new Common.BusinessObject.UserAuthenticationRequestBO();
 
-                Epi.Web.BLL.Publisher Implementation = new Epi.Web.BLL.Publisher(SurveyInfoDao, OrganizationDao);
-                SurveyInfoBO surveyInfoBO = Mapper.ToBusinessObject(pRequest.SurveyInfo);
-                SurveyRequestResultBO surveyRequestResultBO = Implementation.RePublishSurvey(surveyInfoBO);
-                result.PublishInfo = Mapper.ToDataTransferObject(surveyRequestResultBO);
+                //Build Survey Response Xml
 
-                return result;
-            }
+                string Xml = Implementation.CreateResponseXml(GetSurveyInfo(request), request);
+                //Validate Response values
+
+                ErrorMessageList = Implementation.ValidateResponse(GetSurveyInfo(request), request);
+
+                if (ErrorMessageList.Count() > 0)
+                    {
+                    response = new PreFilledAnswerResponse();
+                    response.ErrorMessageList = ErrorMessageList;
+                    response.Status = "Failed";
+                    }
+                else
+                    {
+                    //Insert Survey Response
+
+                    SurveyResponse = Implementation.InsertSurveyResponse(Mapper.ToBusinessObject(Xml, request.AnswerInfo.SurveyId.ToString()));
+
+                    //Save PassCode
+
+                    UserAuthenticationRequestBO = Mapper.ToBusinessObject(SurveyResponse.ResponseId);
+                    Implementation.SavePassCode(UserAuthenticationRequestBO);
+
+                    //return Response
+                    string ResponseUrl = ConfigurationManager.AppSettings["ResponseURL"];
+                    response = new PreFilledAnswerResponse(Mapper.ToDataTransferObjects(UserAuthenticationRequestBO));
+                    response.SurveyResponseUrl = ResponseUrl + UserAuthenticationRequestBO.ResponseId;
+                    response.Status = "Success";
+
+                    }
+                return response;
+                }
             catch (Exception ex)
-            {
-                CustomFaultException customFaultException = new CustomFaultException();
-                customFaultException.CustomMessage = ex.Message;
-                customFaultException.Source = ex.Source;
-                customFaultException.StackTrace = ex.StackTrace;
-                customFaultException.HelpLink = ex.HelpLink;
-                throw new FaultException<CustomFaultException>(customFaultException);
+                {
+                PassCodeDTO DTOList = new Common.DTO.PassCodeDTO();
+                PreFilledAnswerResponse response = new PreFilledAnswerResponse(DTOList);
+
+                response.ErrorMessageList.Add("Failed", "Failed to insert Response");
+                response.Status = "Failed";
+                return response;
+                }
             }
+
+
+        private static List<SurveyInfoBO> GetSurveyInfo(PreFilledAnswerRequest request)
+            {
+            string OrganizationId = "";
+            List<string> SurveyIdList = new List<string>();
+            OrganizationId = request.AnswerInfo.OrganizationKey.ToString();
+            SurveyIdList.Add(request.AnswerInfo.SurveyId.ToString());
+            List<SurveyInfoBO> SurveyBOList = new List<SurveyInfoBO>();
+            SurveyInfoRequest pRequest = new SurveyInfoRequest();
+            var criteria = pRequest.Criteria as SurveyInfoCriteria;
+
+            Epi.Web.Interfaces.DataInterfaces.IDaoFactory entityDaoFactory = new EF.EntityDaoFactory();
+            Epi.Web.Interfaces.DataInterfaces.ISurveyInfoDao surveyInfoDao = entityDaoFactory.SurveyInfoDao;
+            Epi.Web.BLL.SurveyInfo implementation = new Epi.Web.BLL.SurveyInfo(surveyInfoDao);
+
+            SurveyBOList = implementation.GetSurveyInfo(SurveyIdList, criteria.ClosingDate, OrganizationId, criteria.SurveyType, criteria.PageNumber, criteria.PageSize);//Default 
+
+            return SurveyBOList;
+
+            }
+
         }
     }
-}
