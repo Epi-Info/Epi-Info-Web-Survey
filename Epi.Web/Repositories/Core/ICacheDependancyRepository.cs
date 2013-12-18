769c2d6fa2146f59220fd7d4697d536eab8140b4
ita3_cp <18171996+cdc-dpbrown@users.noreply.github.com>
2013-12-18 19:46:19 +0000

    Added files needed by the proxy cache dependency code that was not picked up in the last checkin.

---
+++ Epi.Web/Repositories/Core/ICacheDependancyRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Epi.Web.Common.Message;
+
+namespace Epi.Web.MVC.Repositories.Core
+{
+    public interface ICacheDependencyRepository: IRepository<Epi.Web.Common.Message.CacheDependencyResponse>
+    {
+        CacheDependencyResponse GetCacheDependencyInfo(CacheDependencyRequest request);
+    }
+}
