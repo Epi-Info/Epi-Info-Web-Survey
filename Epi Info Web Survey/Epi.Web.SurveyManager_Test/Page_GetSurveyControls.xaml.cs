76fcdbdca362eaf956bbef028304ab2931e89d57
mlamtahri_cp <SND\mlamtahri_cp>
2014-01-28 15:14:57 +0000

    EIWS: Code refactoring.

--- Epi Info Web Survey/Epi.Web.SurveyManager_Test/Page_GetSurveyControls.xaml.cs
+++ Epi Info Web Survey/Epi.Web.SurveyManager_Test/Page_GetSurveyControls.xaml.cs
@@ -45,13 +45,13 @@ namespace Epi.Web.SurveyManager.Client
 
         private void button3_Click(object sender, RoutedEventArgs e)
             {
-            //SurveyId
+            Epi.Web.Common.Message.SurveyControlsResponse Result = new Common.Message.SurveyControlsResponse();
             try
                 {
                 SurveyManagerServiceV2.ManagerServiceV2Client client = ServiceClient.GetClientV2();
                 Epi.Web.Common.Message.SurveyControlsRequest Request = new Common.Message.SurveyControlsRequest();
                 Request.SurveyId = this.SurveyId.Text;
-                Epi.Web.Common.Message.SurveyControlsResponse Result = client.GetSurveyControlList(Request);
+                  Result = client.GetSurveyControlList(Request);
                 foreach (Epi.Web.Common.DTO.SurveyControlDTO ControlInfo in Result.SurveyControlList)
                     {
                     ControlsInfo.AppendText(string.Format("{0} - {1} - {2}\n", ControlInfo.ControlId, ControlInfo.ControlType, ControlInfo.ControlPrompt));
@@ -61,7 +61,7 @@ namespace Epi.Web.SurveyManager.Client
             catch (Exception ex)
                 {
                 ControlsInfo.AppendText("Error");
-                throw ex;
+                //throw ex;
 
 
 
