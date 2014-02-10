54e4e818c624b66ca2aa65eb24de5a8b410311b1
ita3_cp <18171996+cdc-dpbrown@users.noreply.github.com>
2014-02-10 16:40:55 +0000

    Fixed defect with the clear command not working on mobile fields.

--- Epi.DynamicForms.Core/Fields/Select.cs
+++ Epi.DynamicForms.Core/Fields/Select.cs
@@ -12,9 +12,6 @@ namespace MvcDynamicForms.Fields
     [Serializable]
     public class Select : ListField
     {
-        /// <summary>
-        /// The number of options to display at a time.
-        /// </summary>
         public int Size
         {
             get
@@ -24,9 +21,7 @@ namespace MvcDynamicForms.Fields
             }
             set { _inputHtmlAttributes["size"] = value.ToString(); }
         }
-        /// <summary>
-        /// Determines whether the select element will accept multiple selections.
-        /// </summary>
+
         public bool MultipleSelection
         {
             get
@@ -41,15 +36,7 @@ namespace MvcDynamicForms.Fields
             set { _inputHtmlAttributes["multiple"] = value.ToString(); }
         }
 
-        //public int SelectType { get; set; }
-        /// <summary>
-        /// The text to be rendered as the first option in the select list when ShowEmptyOption is set to true.
-        /// </summary>
-        /// 
         public string EmptyOption { get; set; }
-        /// <summary>
-        /// Determines whether a valueless option is rendered as the first option in the list.
-        /// </summary>
         public bool ShowEmptyOption { get; set; }
 
         public override string RenderHtml()
@@ -59,21 +46,15 @@ namespace MvcDynamicForms.Fields
             var inputName = _fieldPrefix + _key;
             string ErrorStyle = string.Empty;
             
-            // prompt
             var prompt = new TagBuilder("label");
-
             System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(@"(\r\n|\r|\n)+");
-
             string newText = regex.Replace(Prompt.Replace(" ", "&nbsp;"), "<br />");
-
             string NewPromp = System.Web.Mvc.MvcHtmlString.Create(newText).ToString();
-
 
             prompt.InnerHtml=NewPromp;
             prompt.Attributes.Add("for", inputName);
             prompt.Attributes.Add("class", "EpiLabel");
             prompt.Attributes.Add("Id", "label" + inputName);
-
             
             StringBuilder StyleValues = new StringBuilder();
             StyleValues.Append(GetContolStyle(_fontstyle.ToString(), _Prompttop.ToString(), _Promptleft.ToString(), null, Height.ToString(),_IsHidden));
@@ -102,64 +83,59 @@ namespace MvcDynamicForms.Fields
             int LargestChoiseLength =0 ;
             string measureString = "";
             
-            foreach (var choise in _choices) 
+            foreach (var choice in _choices) 
             {
-                if (choise.Key.ToString().Length > LargestChoiseLength) 
+                if (choice.Key.ToString().Length > LargestChoiseLength) 
                 {
-                    LargestChoiseLength = choise.Key.ToString().Length;
-                    measureString = choise.Key.ToString();
+                    LargestChoiseLength = choice.Key.ToString().Length;
+                    measureString = choice.Key.ToString();
                 } 
             }
-
-           // LargestChoiseLength = LargestChoiseLength * _ControlFontSize;
 
             Font stringFont = new Font(ControlFontStyle, _ControlFontSize);
 
             SizeF size = new SizeF() ;
+            
             using (Graphics g = Graphics.FromHwnd(IntPtr.Zero))
             {
-                  size = g.MeasureString(measureString.ToString(), stringFont);
+                size = g.MeasureString(measureString.ToString(), stringFont);
             }
-
-
-           
-          // stringSize = (int) Graphics.MeasureString(measureString.ToString(), stringFont).Width;
-        
 
             if (_IsRequired == true)
             {
-                        if ((size.Width) > _ControlWidth)
-                        {
-                           // select.Attributes.Add("class", GetControlClass() + "text-input fix-me");
-                            select.Attributes.Add("class", GetControlClass() + "fix-me");
-                        }
-                        else
-                        {
-                           // select.Attributes.Add("class", GetControlClass() + "text-input");
-                            select.Attributes.Add("class", GetControlClass()  );
-                        }
-                       select.Attributes.Add("data-prompt-position", "topRight:10");
+                if ((size.Width) > _ControlWidth)
+                {
+                    select.Attributes.Add("class", GetControlClass() + "fix-me");
+                }
+                else
+                {
+                    select.Attributes.Add("class", GetControlClass()  );
+                }
+
+                select.Attributes.Add("data-prompt-position", "topRight:10");
             }
             else 
             {
-                        //select.Attributes.Add("class", GetControlClass() + "text-input fix-me");
                 if ((size.Width) > _ControlWidth)
                 {
                     select.Attributes.Add("class", GetControlClass() +"fix-me ");
                 }
                 else
                 {
-
                     select.Attributes.Add("class", GetControlClass() );
                 }
+                
                 select.Attributes.Add("data-prompt-position", "topRight:10");
                  
             }
             string IsHiddenStyle = "";
             string IsHighlightedStyle = "";
-            if(_IsHidden){
+
+            if(_IsHidden)
+            {
                 IsHiddenStyle = "display:none";
             }
+            
             if (_IsHighlighted)
             {
                 IsHighlightedStyle = "background-color:yellow";
@@ -169,11 +145,12 @@ namespace MvcDynamicForms.Fields
             {
                 select.Attributes.Add("disabled", "disabled");
             }
+            
             string InputFieldStyle = GetInputFieldStyle(_InputFieldfontstyle.ToString(), _InputFieldfontSize, _InputFieldfontfamily.ToString());
             select.Attributes.Add("style", "position:absolute;left:" + _left.ToString() + "px;top:" + _top.ToString() + "px" + ";width:" + _ControlWidth.ToString() + "px ; font-size:" + _ControlFontSize + "pt;" + ErrorStyle + ";" + IsHiddenStyle + ";" + IsHighlightedStyle + ";" + InputFieldStyle);
             select.MergeAttributes(_inputHtmlAttributes);
             html.Append(select.ToString(TagRenderMode.StartTag));
-            // If readonly then add the following jquery script to make the field disabled 
+
             if (ReadOnly)
             {
                 var scriptReadOnlyText = new TagBuilder("script");
@@ -181,7 +158,6 @@ namespace MvcDynamicForms.Fields
                 html.Append(scriptReadOnlyText.ToString(TagRenderMode.Normal));
             }
 
-            // initial empty option
             if (ShowEmptyOption)
             {
                 var opt = new TagBuilder("option");
@@ -189,8 +165,6 @@ namespace MvcDynamicForms.Fields
                 opt.SetInnerText(EmptyOption);
                 html.Append(opt.ToString());
             }
-
-            // options
 
             switch (this.SelectType.ToString())
             {
@@ -214,7 +188,6 @@ namespace MvcDynamicForms.Fields
                             opt.SetInnerText(choice.Key);
                             html.Append(opt.ToString());
                         }
-                        
                     }
                     break;
                 case "17":
@@ -265,10 +238,8 @@ namespace MvcDynamicForms.Fields
                     break;
             }
  
-            // close select element
             html.Append(select.ToString(TagRenderMode.EndTag));
 
-            // add hidden tag, so that a value always gets sent for select tags
             var hidden = new TagBuilder("input");
             hidden.Attributes.Add("type", "hidden");
             hidden.Attributes.Add("id", inputName + "_hidden");
@@ -287,24 +258,19 @@ namespace MvcDynamicForms.Fields
             wrapper.InnerHtml = html.ToString();
             return wrapper.ToString();
         }
+
         public string GetControlClass()
         {
-
             StringBuilder ControlClass = new StringBuilder();
-
             ControlClass.Append("validate[");
-
 
             if (_IsRequired == true)
             {
-
                 ControlClass.Append("required");
-
             }
+
             ControlClass.Append("]");
-
             return ControlClass.ToString();
-
         }
     }
 }
