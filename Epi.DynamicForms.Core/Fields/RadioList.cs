a3643978fdd2619dff7a5c24087d1d8142794e1c
ita3_cp <18171996+cdc-dpbrown@users.noreply.github.com>
2014-04-17 20:16:59 +0000

    Fixed defect with radio button click event.

--- Epi.DynamicForms.Core/Fields/RadioList.cs
+++ Epi.DynamicForms.Core/Fields/RadioList.cs
@@ -113,14 +113,9 @@ namespace MvcDynamicForms.Fields
                 radioTag.Attributes.Add("name", inputName);
                 radioTag.Attributes.Add("class", inputName);
 
-                if (FunctionObjectAfter != null && !FunctionObjectAfter.IsNull())
-                {
-                    radioTag.Attributes.Add("onblur", "return " + _key + "_after();"); 
-                }
-
                 if (FunctionObjectClick != null && !FunctionObjectClick.IsNull())
                 {
-                    radioTag.Attributes.Add("onclick", "return " + _key + "_click();");
+                    radioTag.Attributes.Add("onclick", "return " + _key + "_after();");
                 }
 
                 radioTag.SetInnerText(choiceList[i].Key);
