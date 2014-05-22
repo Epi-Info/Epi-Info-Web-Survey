09c59d292df876c94252fa5c7b4257331daba182
ita3_cp <SND\ita3_cp>
2014-05-22 16:42:44 +0000

    Branch Web1.2.1.0 - Merged in the following change sets from the main trunk. 105150, 105154, 105160, 105163, 105297

--- Web1.2.1.0/Epi.Web/Controllers/PostController.cs
+++ Web1.2.1.0/Epi.Web/Controllers/PostController.cs
@@ -12,22 +12,20 @@ namespace Epi.Web.Controllers
 {
     public class PostController : Controller
     {
-        //
-        // GET: /Notify/
-
         [AcceptVerbs(HttpVerbs.Post)]
-       [ValidateAntiForgeryToken]
-        public JsonResult Notify(string emailAddress, string redirectUrl, string surveyName,string passCode, string EmailSubject)
+        [ValidateAntiForgeryToken]
+        public JsonResult Notify(string emailAddress, string redirectUrl, string surveyName, string passCode, string emailSubject)
         {
             try
             {
                 Epi.Web.Common.Email.Email EmailObj = new Common.Email.Email();
                 EmailObj.Body = redirectUrl + " and Pass Code is: " + passCode;
                 EmailObj.From = ConfigurationManager.AppSettings["EMAIL_FROM"].ToString();
-                EmailObj.Subject = "Link for Survey: " + surveyName;// EmailSubject;
-                List<string> tempList = new List<string>(); 
+                EmailObj.Subject = emailSubject;
+
+                List<string> tempList = new List<string>();
                 tempList.Add(emailAddress);
-                EmailObj.To = tempList ;
+                EmailObj.To = tempList;
 
                 if (Epi.Web.Common.Email.EmailHandler.SendMessage(EmailObj))
                 {
@@ -37,21 +35,14 @@ namespace Epi.Web.Controllers
                 {
                     return Json(false);
                 }
-             }
+            }
             catch (Exception ex)
             {
                 return Json(false);
             }
         }
 
-
-        /// <summary>
-        /// Sign out a Survey Instance
-        /// </summary>
-        /// <returns></returns>
         [HttpPost]
-        
-      // [ValidateAntiForgeryToken]
         public JsonResult SignOut()
         {
             try
@@ -59,29 +50,10 @@ namespace Epi.Web.Controllers
                 FormsAuthentication.SignOut();
                 return Json(true);
             }
-            catch (Exception ex)
+            catch
             {
                 return Json(false);
             }
         }
-
-        public static string UnescapeCodes(string src)
-        {
-           src = Uri.UnescapeDataString(src);
-           var rx = new Regex("\\\\([0-9A-Fa-f]+)");
-            var res = new StringBuilder();
-            var pos = 0;
-            foreach (Match m in rx.Matches(src))
-            {
-                res.Append(src.Substring(pos, m.Index));
-                pos = m.Index + m.Length;
-                res.Append((char)Convert.ToInt32(m.Groups[1].ToString(), 16));
-            }
-            res.Append(src.Substring(pos));
-
-
-            return res.ToString();
-        }
-      
     }
 }
