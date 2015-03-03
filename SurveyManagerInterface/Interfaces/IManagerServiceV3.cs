e183cb99121f533829b1fd26412bf707b1b866d5
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2015-03-03 16:04:47 +0000

    EIWS:I exposed a new service method that is used to update a record status when the record is downloaded. I also made some changes to the query used to get response; I incorporate (status and IsdraftMode) to the query. Now when a record is downloaded the record status will be 4.

---
+++ SurveyManagerInterface/Interfaces/IManagerServiceV3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Epi.Web.Common.DTO;
+using Epi.Web.Common.Message;
+using Epi.Web.Common.Exception;
+namespace Epi.Web.WCF.SurveyService
+{
+    [ServiceContract]
+    interface IManagerServiceV3:IManagerServiceV2
+    {
+        [OperationContract]
+        [FaultContract(typeof(CustomFaultException))]
+        void UpdateRecordStatus(SurveyAnswerRequest pRequestMessage);
+    }
+}
