148ea162810493a18f2b151c1097c1d48362a812
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2015-03-03 16:04:47 +0000

    EIWS:I exposed a new service method that is used to update a record status when the record is downloaded. I also made some changes to the query used to get response; I incorporate (status and IsdraftMode) to the query. Now when a record is downloaded the record status will be 4.

---
+++ SurveyManagerInterface/ManagerServiceV3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Epi.Web.Common.DTO;
+using Epi.Web.Common.Message;
+using Epi.Web.Common.MessageBase;
+using Epi.Web.Common.Criteria;
+using Epi.Web.Common.ObjectMapping;
+using Epi.Web.Common.BusinessObject;
+using Epi.Web.Common.Exception;
+using Epi.Web.BLL;
+using System.Xml;
+using System.Xml.Linq;
+using System.Configuration;
+using Epi.Web.Common.Security;
+
+namespace Epi.Web.WCF.SurveyService
+{
+    public class ManagerServiceV3 : ManagerServiceV2, IManagerServiceV3
+    {
+       public void UpdateRecordStatus(SurveyAnswerRequest pRequestMessage)
+       {
+           try
+           {
+               Epi.Web.Interfaces.DataInterfaces.ISurveyResponseDao SurveyResponseDao = new EF.EntitySurveyResponseDao();
+               Epi.Web.BLL.SurveyResponse Implementation = new Epi.Web.BLL.SurveyResponse(SurveyResponseDao);
+               foreach (SurveyAnswerDTO DTO in pRequestMessage.SurveyAnswerList)
+               {
+                   Implementation.UpdateRecordStatus(Mapper.ToBusinessObject(DTO));
+               }
+                
+           }
+           catch (Exception ex)
+           {
+
+               throw ex;
+               
+                
+           }
+       }
+    }
+}
