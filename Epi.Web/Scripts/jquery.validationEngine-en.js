068d149a3781681e8814f9e4cc6ab185e3b68a31
mlamtahri_cp <24397119+mlamtahri@users.noreply.github.com>
2015-03-18 14:05:54 +0000

    EIWS: Fixed issue related to Time validation not firing properly.

--- Epi.Web/Scripts/jquery.validationEngine-en.js
+++ Epi.Web/Scripts/jquery.validationEngine-en.js
@@ -99,7 +99,8 @@
                 },
 
                 "time": {
-                 "regex": /^(\d{1,2}):(\d{2})(:(\d{2}))?(\s?(AM|am|PM|pm))?$/,
+                    // "regex": /^(\d{1,2}):(\d{2})(:(\d{2}))?(\s?(AM|am|PM|pm))?$/,
+                    "regex": /^\d{2,}:(?:[0-5]\d):(?:[0-5]\d)$/ ,
                  "alertText": "* Invalid time format"
                 },
 
