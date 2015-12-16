de069091563cf198a0c6788026d0241af681702d
mlamtahri_cp <SND\mlamtahri_cp>
2015-12-16 18:47:46 +0000

    EIWS: Manager client changes...

--- Epi Info Web Survey/Epi.Web.SurveyManager_Test/ServiceClient.cs
+++ Epi Info Web Survey/Epi.Web.SurveyManager_Test/ServiceClient.cs
@@ -16,6 +16,7 @@ namespace Epi.Web.SurveyManager.Client
             {
             SurveyManagerService = 1,
             SurveyManagerServiceV2 = 2,
+            SurveyManagerServiceV3 = 3,
             }
         public static SurveyManagerService.ManagerServiceClient GetClient(string pEndPointAddress, bool pIsAuthenticated, bool pIsWsHttpBinding = true)
         {
@@ -459,6 +460,214 @@ namespace Epi.Web.SurveyManager.Client
                 }
             return result;
             }
+        public static SurveyManagerServiceV3.ManagerServiceV3Client GetClientV3()
+        {
+            string pEndPointAddress = ConfigurationManager.AppSettings["EndPointAddress"];
+            bool pIsAuthenticated = false;
+            bool pIsWsHTTPBinding = true;
+            string s = ConfigurationManager.AppSettings["Authentication_Use_Windows"];
+            if (!String.IsNullOrEmpty(s))
+            {
+                if (s.ToUpper() == "TRUE")
+                {
+                    pIsAuthenticated = true;
+                }
+            }
+
+
+            s = ConfigurationManager.AppSettings["WCF_BINDING_TYPE"];
+            if (!String.IsNullOrEmpty(s))
+            {
+                if (s.ToUpper() == "WSHTTP")
+                {
+                    pIsWsHTTPBinding = true;
+                }
+                else
+                {
+                    pIsWsHTTPBinding = false;
+                }
+            }
+
+            return GetClientV3(pEndPointAddress, pIsAuthenticated, pIsWsHTTPBinding);
+        }
+        public static SurveyManagerServiceV3.ManagerServiceV3Client GetClientV3(string pEndPointAddress, bool pIsAuthenticated, bool pIsWsHttpBinding = true)
+        {
+            SurveyManagerServiceV3.ManagerServiceV3Client result = null;
+            try
+            {
+                if (pIsAuthenticated) // Windows Authentication
+                {
+                    System.ServiceModel.BasicHttpBinding binding = new System.ServiceModel.BasicHttpBinding();
+                    binding.Name = "BasicHttpBinding";
+                    binding.CloseTimeout = new TimeSpan(0, 1, 0);
+                    binding.OpenTimeout = new TimeSpan(0, 1, 0);
+                    binding.ReceiveTimeout = new TimeSpan(0, 10, 0);
+                    binding.SendTimeout = new TimeSpan(0, 1, 0);
+                    binding.AllowCookies = false;
+                    binding.BypassProxyOnLocal = false;
+                    binding.HostNameComparisonMode = System.ServiceModel.HostNameComparisonMode.StrongWildcard;
+                    binding.MaxBufferPoolSize = long.Parse(ConfigurationManager.AppSettings["MaxBufferPoolSize"]);//524288;
+                    binding.MaxReceivedMessageSize = long.Parse(ConfigurationManager.AppSettings["MaxReceivedMessageSize"]);
+                    binding.MessageEncoding = System.ServiceModel.WSMessageEncoding.Text;
+                    binding.TextEncoding = System.Text.Encoding.UTF8;
+                    binding.TransferMode = System.ServiceModel.TransferMode.Buffered;
+                    binding.UseDefaultWebProxy = true;
+                    binding.ReaderQuotas.MaxDepth = int.Parse(ConfigurationManager.AppSettings["MaxDepth"]);//32;
+                    binding.ReaderQuotas.MaxStringContentLength = int.Parse(ConfigurationManager.AppSettings["MaxStringContentLength"]);  //8192;
+                    binding.ReaderQuotas.MaxArrayLength = int.Parse(ConfigurationManager.AppSettings["MaxArrayLength"]); //16384;
+                    binding.ReaderQuotas.MaxBytesPerRead = int.Parse(ConfigurationManager.AppSettings["MaxBytesPerRead"]); //4096;
+                    binding.ReaderQuotas.MaxNameTableCharCount = int.Parse(ConfigurationManager.AppSettings["MaxNameTableCharCount"]); //16384;
+                    System.ServiceModel.EndpointAddress endpoint = new System.ServiceModel.EndpointAddress(pEndPointAddress);
+                    if (endpoint.Uri.Scheme == "http")
+                    {
+                        binding.Security.Mode = System.ServiceModel.BasicHttpSecurityMode.TransportCredentialOnly;
+                        binding.Security.Transport.ClientCredentialType = System.ServiceModel.HttpClientCredentialType.Windows;
+                        binding.Security.Transport.ProxyCredentialType = System.ServiceModel.HttpProxyCredentialType.None;
+                        binding.Security.Transport.Realm = string.Empty;
+                    }
+                    else
+                    {
+                        binding.Security.Mode = System.ServiceModel.BasicHttpSecurityMode.Transport;
+                        binding.Security.Transport.ClientCredentialType = System.ServiceModel.HttpClientCredentialType.None;
+
+
+                    }
+
+                    binding.Security.Message.ClientCredentialType = System.ServiceModel.BasicHttpMessageCredentialType.UserName;
+
+                    result = new SurveyManagerServiceV3.ManagerServiceV3Client(binding, endpoint);
+                    result.ClientCredentials.Windows.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
+                    result.ChannelFactory.Credentials.Windows.ClientCredential = System.Net.CredentialCache.DefaultNetworkCredentials;
+                }
+                else
+                {
+                    if (pIsWsHttpBinding)
+                    {
+                        System.ServiceModel.WSHttpBinding binding = new System.ServiceModel.WSHttpBinding();
+                        binding.Name = "WSHttpBinding";
+                        binding.CloseTimeout = new TimeSpan(0, 1, 0);
+                        binding.OpenTimeout = new TimeSpan(0, 1, 0);
+                        binding.ReceiveTimeout = new TimeSpan(0, 10, 0);
+                        binding.SendTimeout = new TimeSpan(0, 1, 0);
+                        binding.BypassProxyOnLocal = false;
+                        binding.TransactionFlow = false;
+                        binding.HostNameComparisonMode = System.ServiceModel.HostNameComparisonMode.StrongWildcard;
+                        binding.MaxBufferPoolSize = long.Parse(ConfigurationManager.AppSettings["MaxBufferPoolSize"]);//524288;
+                        binding.MaxReceivedMessageSize = long.Parse(ConfigurationManager.AppSettings["MaxReceivedMessageSize"]);
+                        binding.MessageEncoding = System.ServiceModel.WSMessageEncoding.Text;
+                        binding.TextEncoding = System.Text.Encoding.UTF8;
+                        binding.UseDefaultWebProxy = true;
+                        binding.AllowCookies = false;
+
+                        binding.ReaderQuotas.MaxDepth = int.Parse(ConfigurationManager.AppSettings["MaxDepth"]);//32;
+                        binding.ReaderQuotas.MaxStringContentLength = int.Parse(ConfigurationManager.AppSettings["MaxStringContentLength"]);  //8192;
+                        binding.ReaderQuotas.MaxArrayLength = int.Parse(ConfigurationManager.AppSettings["MaxArrayLength"]); //16384;
+                        binding.ReaderQuotas.MaxBytesPerRead = int.Parse(ConfigurationManager.AppSettings["MaxBytesPerRead"]); //4096;
+                        binding.ReaderQuotas.MaxNameTableCharCount = int.Parse(ConfigurationManager.AppSettings["MaxNameTableCharCount"]); //16384;
+
+                        binding.ReliableSession.Ordered = true;
+                        binding.ReliableSession.InactivityTimeout = new TimeSpan(0, 10, 0);
+                        binding.ReliableSession.Enabled = false;
+
+                        System.ServiceModel.EndpointAddress endpoint = new System.ServiceModel.EndpointAddress(pEndPointAddress);
+                        if (endpoint.Uri.Scheme == "http")
+                        {
+                            binding.Security.Mode = System.ServiceModel.SecurityMode.Message;
+                            binding.Security.Transport.ClientCredentialType = System.ServiceModel.HttpClientCredentialType.Windows;
+                            binding.Security.Transport.ProxyCredentialType = System.ServiceModel.HttpProxyCredentialType.None;
+                            binding.Security.Transport.Realm = string.Empty;
+
+                        }
+                        else
+                        {
+                            // binding.Security.Mode = System.ServiceModel.BasicHttpSecurityMode.Transport;
+                            //  binding.Security.Transport.ClientCredentialType = System.ServiceModel.HttpClientCredentialType.None;
+
+
+                        }
+
+
+                        binding.Security.Message.ClientCredentialType = System.ServiceModel.MessageCredentialType.Windows;
+                        binding.Security.Message.NegotiateServiceCredential = true;
+
+
+                        result = new SurveyManagerServiceV3.ManagerServiceV3Client(binding, endpoint);
+
+
+                    }
+                    else
+                    {
+                        System.ServiceModel.BasicHttpBinding binding = new System.ServiceModel.BasicHttpBinding();
+                        binding.Name = "BasicHttpBinding";
+                        binding.CloseTimeout = new TimeSpan(0, 1, 0);
+                        binding.OpenTimeout = new TimeSpan(0, 1, 0);
+                        binding.ReceiveTimeout = new TimeSpan(0, 10, 0);
+                        binding.SendTimeout = new TimeSpan(0, 1, 0);
+                        binding.AllowCookies = false;
+                        binding.BypassProxyOnLocal = false;
+                        binding.HostNameComparisonMode = System.ServiceModel.HostNameComparisonMode.StrongWildcard;
+                        binding.MaxBufferPoolSize = long.Parse(ConfigurationManager.AppSettings["MaxBufferPoolSize"]);//524288;
+                        binding.MaxReceivedMessageSize = long.Parse(ConfigurationManager.AppSettings["MaxReceivedMessageSize"]);
+                        binding.MessageEncoding = System.ServiceModel.WSMessageEncoding.Text;
+                        binding.TextEncoding = System.Text.Encoding.UTF8;
+                        binding.TransferMode = System.ServiceModel.TransferMode.Buffered;
+                        binding.UseDefaultWebProxy = true;
+                        binding.ReaderQuotas.MaxDepth = int.Parse(ConfigurationManager.AppSettings["MaxDepth"]);//32;
+                        binding.ReaderQuotas.MaxStringContentLength = int.Parse(ConfigurationManager.AppSettings["MaxStringContentLength"]);  //8192;
+                        binding.ReaderQuotas.MaxArrayLength = int.Parse(ConfigurationManager.AppSettings["MaxArrayLength"]); //16384;
+                        binding.ReaderQuotas.MaxBytesPerRead = int.Parse(ConfigurationManager.AppSettings["MaxBytesPerRead"]); //4096;
+                        binding.ReaderQuotas.MaxNameTableCharCount = int.Parse(ConfigurationManager.AppSettings["MaxNameTableCharCount"]); //16384;
+                        System.ServiceModel.EndpointAddress endpoint = new System.ServiceModel.EndpointAddress(pEndPointAddress);
+                        if (endpoint.Uri.Scheme == "http")
+                        {
+                            binding.Security.Mode = System.ServiceModel.BasicHttpSecurityMode.TransportCredentialOnly;
+                            binding.Security.Transport.ClientCredentialType = System.ServiceModel.HttpClientCredentialType.Windows;
+                            binding.Security.Transport.ProxyCredentialType = System.ServiceModel.HttpProxyCredentialType.None;
+                            binding.Security.Transport.Realm = string.Empty;
+                        }
+                        else
+                        {
+                            binding.Security.Mode = System.ServiceModel.BasicHttpSecurityMode.Transport;
+                            binding.Security.Transport.ClientCredentialType = System.ServiceModel.HttpClientCredentialType.None;
+
+
+                        }
+
+                        result = new SurveyManagerServiceV3.ManagerServiceV3Client(binding, endpoint);
+                        System.Net.ServicePointManager.ServerCertificateValidationCallback +=
+                           (se, cert, chain, sslerror) =>
+                           {
+                               return true;
+                           };
+                    }
+                }
+            }
+            catch (FaultException<CustomFaultException> cfe)
+            {
+                throw cfe;
+            }
+            catch (FaultException fe)
+            {
+                throw fe;
+            }
+            catch (SecurityNegotiationException sne)
+            {
+                throw sne;
+            }
+            catch (CommunicationException ce)
+            {
+                throw ce;
+            }
+            catch (TimeoutException te)
+            {
+                throw te;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return result;
+        }
 
         public static int GetServiceVersion() {
 
