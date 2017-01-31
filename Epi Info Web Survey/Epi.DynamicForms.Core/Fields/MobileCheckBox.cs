6583b5d96fee16b0fe65d507e5fd152ec1b0be4b
sparitala_cp <SND\sparitala_cp>
2017-01-31 17:19:03 +0000

    EW-291 Prompt font for mobile controls does not reflect in mobile view EW-290 Default prompt font of label in mobile view relatively smaller then that of other controls

--- Epi Info Web Survey/Epi.DynamicForms.Core/Fields/MobileCheckBox.cs
+++ Epi Info Web Survey/Epi.DynamicForms.Core/Fields/MobileCheckBox.cs
@@ -47,8 +47,8 @@ namespace MvcDynamicForms.Fields
             //{
             //    checkboxTag.Attributes.Add("disabled", "disabled");
             //}
-
-            checkboxTag.Attributes.Add("style", "" + ErrorStyle + ";" + IsHiddenStyle + ";" + IsHighlightedStyle);
+           
+            checkboxTag.Attributes.Add("style", "" + ErrorStyle + ";" + IsHiddenStyle + ";" + IsHighlightedStyle );
             checkboxTag.MergeAttributes(_inputHtmlAttributes);
 
             var FunctionCalls ="";
@@ -86,6 +86,9 @@ namespace MvcDynamicForms.Fields
             prompt.Attributes.Add("for", inputName);
             prompt.Attributes.Add("class", "EpiLabel");
             prompt.Attributes.Add("Id", "label" + inputName);
+            StringBuilder StyleValues = new StringBuilder();
+            StyleValues.Append(GetControlStyle(_fontstyle.ToString(), _Prompttop.ToString(), _Promptleft.ToString(), null, Height.ToString(), _IsHidden));
+            prompt.Attributes.Add("style", StyleValues.ToString());
             html.Append(prompt.ToString());
 
             //if (ReadOnly)
@@ -105,7 +108,7 @@ namespace MvcDynamicForms.Fields
             hdn.Attributes.Add("type", "hidden");
             hdn.Attributes.Add("id", inputName + "_hidden");
             hdn.Attributes.Add("name", inputName);
-            hdn.Attributes.Add("value", bool.FalseString);
+            hdn.Attributes.Add("value", bool.FalseString);           
             html.Append(hdn.ToString(TagRenderMode.SelfClosing));
 
             //prevent check box control to submit on enter click
@@ -126,6 +129,85 @@ namespace MvcDynamicForms.Fields
             wrapper.Attributes["id"] = inputName + "_fieldWrapper";
             wrapper.InnerHtml = html.ToString();
             return wrapper.ToString();
+        }
+        public override string GetControlStyle(string ControlFontStyle, string Top, string Left, string Width, string Height, bool IsHidden)
+        {
+            StringBuilder FontStyle = new StringBuilder();
+            StringBuilder FontWeight = new StringBuilder();
+            StringBuilder TextDecoration = new StringBuilder();
+            StringBuilder CssStyles = new StringBuilder();
+
+            char[] delimiterChars = { ' ', ',' };
+            string[] Styles = ControlFontStyle.Split(delimiterChars);
+           // CssStyles.Append("width: auto");
+
+            foreach (string Style in Styles)
+            {
+                switch (Style.ToString())
+                {
+                    case "Italic":
+                        FontStyle.Append(Style.ToString());
+                        break;
+                    case "Oblique":
+                        FontStyle.Append(Style.ToString());
+                        break;
+                }
+
+            }
+
+            foreach (string Style in Styles)
+            {
+                switch (Style.ToString())
+                {
+                    case "Bold":
+                        FontWeight.Append(Style.ToString());
+                        break;
+                    case "Normal":
+                        FontWeight.Append(Style.ToString());
+                        break;
+                }
+            }
+
+            CssStyles.Append(" font:");//1
+
+            if (!string.IsNullOrEmpty(FontStyle.ToString()))
+            {
+                CssStyles.Append(FontStyle);//2
+                CssStyles.Append(" ");//3
+            }
+
+            CssStyles.Append(FontWeight);
+            CssStyles.Append(" ");
+            CssStyles.Append(_fontSize.ToString() + "pt ");
+            CssStyles.Append(" ");
+            CssStyles.Append(_fontfamily.ToString());
+
+            foreach (string Style in Styles)
+            {
+                switch (Style.ToString())
+                {
+                    case "Strikeout":
+                        TextDecoration.Append("line-through");
+                        break;
+                    case "Underline":
+                        TextDecoration.Append(Style.ToString());
+                        break;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(TextDecoration.ToString()))
+            {
+                CssStyles.Append(";text-decoration:");
+            }
+
+            if (IsHidden)
+            {
+                CssStyles.Append(";display:none");
+            }
+
+            CssStyles.Append(TextDecoration);
+
+            return CssStyles.ToString();
         }
     }
 }
