0e6f0c80c1d16f8f24b02b5ff2fc92cee2577ccf
ParitalaS <ParitalaS@A26945>
2018-07-11 12:17:10 -0400

    SurveyAPI Integration

---
+++ Epi.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using Epi.Web.MVC.Resolver;
+using Epi.Web.SurveyAPI.Repository;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace Epi.Web.MVC.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            var container = new UnityContainer();
+            container.RegisterType<ISurveyResponseRepository, SurveyResponseRepository>(new HierarchicalLifetimeManager());
+            configuration.DependencyResolver = new UnityResolver(container);
+        }
+    }
+}
