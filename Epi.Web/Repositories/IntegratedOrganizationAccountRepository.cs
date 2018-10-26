5d40e69914c18c8cd7db8511bbed4b32be4b9cfa
ParitalaS <ParitalaS@A26945>
2018-10-26 16:16:43 -0400

    Update Epi.Web/Repositories/IntegratedOrganizationAccountRepository.cs
    test commit

--- Epi.Web/Repositories/IntegratedOrganizationAccountRepository.cs
+++ Epi.Web/Repositories/IntegratedOrganizationAccountRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Epi.Web.MVC.Repositories.Core;
-using Epi.Web.MVC.DataServiceClient;
+//using Epi.Web.MVC.DataServiceClient;
 using Epi.Web.Common.Message;
 using Epi.Web.Common.Exception;
 using System.ServiceModel;
