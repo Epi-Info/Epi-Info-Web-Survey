5351ac1a15e4a30b537432b39f8692fa745ac23d
ParitalaS <ParitalaS@A26945>
2019-04-30 16:35:01 -0400

    SurveyJson

--- Epi.Web.Common/Json/SurveyJson.cs
+++ Epi.Web.Common/Json/SurveyJson.cs
@@ -17,133 +17,139 @@ namespace Epi.Web.Common.Json
         public string GetSurveyResponseJson(Epi.Web.Common.DTO.SurveyAnswerDTO surveyAnswer, List<FormsHierarchyDTO> FormsHierarchyDTOList, Dictionary<string, SurveyControlsResponse> List)
         {
             ResponseDetail Responsedetail = new ResponseDetail();
-
+            var json = "";
             var ChildFormsHierarchy = FormsHierarchyDTOList.Where(x => x.IsRoot == false);
             Dictionary<string, object> ResponseQA = new Dictionary<string, object>();
             Dictionary<string, object> RootResponseQA = new Dictionary<string, object>();
-
-            XDocument xdoc = XDocument.Parse(surveyAnswer.XML);
-            int NumberOfPages = GetNumberOfPags(surveyAnswer.XML);           
-
-            Responsedetail.ResponseId = surveyAnswer.ResponseId;
-            Responsedetail.FormId = surveyAnswer.SurveyId;
-            SurveyControlsResponse surveylist= List.Where(x => x.Key == surveyAnswer.SurveyId).Select(d => d.Value).Single();
-            
-            Responsedetail.OKey = FormsHierarchyDTOList[0].SurveyInfo.OrganizationKey.ToString().Substring(0, 8);
-            for (int i = 1; NumberOfPages + 1 > i; i++)
+            if (!string.IsNullOrEmpty(surveyAnswer.XML))
             {
-                var _FieldsTypeIDs = from _FieldTypeID in
-               xdoc.Descendants("Page")
+                XDocument xdoc = XDocument.Parse(surveyAnswer.XML);
+                int NumberOfPages = GetNumberOfPags(surveyAnswer.XML);
 
-                                     where _FieldTypeID.Attribute("PageNumber").Value == (i).ToString()
-                                     select _FieldTypeID;
+                Responsedetail.ResponseId = surveyAnswer.ResponseId;
+                Responsedetail.FormId = surveyAnswer.SurveyId;
+                SurveyControlsResponse surveylist = List.Where(x => x.Key == surveyAnswer.SurveyId).Select(d => d.Value).Single();
 
-                var _PageFieldsTypeIDs = from _FieldTypeID1 in
-                                             _FieldsTypeIDs.Descendants("ResponseDetail")
-
-                                         select _FieldTypeID1;
-                foreach (var item in _PageFieldsTypeIDs)
+                Responsedetail.OKey = FormsHierarchyDTOList[0].SurveyInfo.OrganizationKey.ToString().Substring(0, 8);
+                for (int i = 1; NumberOfPages + 1 > i; i++)
                 {
-                    string ControlId = item.Attribute("QuestionName").Value;
-                    bool IsCheckBox = (bool)surveylist.SurveyControlList.Any(x => x.ControlId == ControlId && x.ControlType == "CheckBox");                   
-                    bool ISNumericTextBox = (bool)surveylist.SurveyControlList.Any(x => x.ControlId == ControlId && x.ControlType == "NumericTextBox");
-                    if (ISNumericTextBox && !string.IsNullOrEmpty(item.Value))
+                    var _FieldsTypeIDs = from _FieldTypeID in
+                   xdoc.Descendants("Page")
+
+                                         where _FieldTypeID.Attribute("PageNumber").Value == (i).ToString()
+                                         select _FieldTypeID;
+
+                    var _PageFieldsTypeIDs = from _FieldTypeID1 in
+                                                 _FieldsTypeIDs.Descendants("ResponseDetail")
+
+                                             select _FieldTypeID1;
+                    foreach (var item in _PageFieldsTypeIDs)
                     {
-                        string uiSep = CultureInfo.CurrentUICulture.NumberFormat.NumberDecimalSeparator;
-                        if (item.Value.Contains(uiSep))                           
-                            RootResponseQA.Add(item.Attribute("QuestionName").Value, Convert.ToDecimal(item.Value)); 
-                        else
-                            RootResponseQA.Add(item.Attribute("QuestionName").Value, Convert.ToInt32(item.Value));
-                    }
-                    else if (IsCheckBox)
-                    {
-                        bool Ischecked = false;
-                        if (item.Value == "Yes")
-                            RootResponseQA.Add(item.Attribute("QuestionName").Value, !Ischecked);
-                        else
-                            RootResponseQA.Add(item.Attribute("QuestionName").Value, Ischecked);
-                    }
-                    else
-                        RootResponseQA.Add(item.Attribute("QuestionName").Value, item.Value);
-                }
-
-            }
-            Responsedetail.ResponseQA = RootResponseQA;
-
-            foreach (var child in ChildFormsHierarchy)
-            {
-                List<SurveyAnswerDTO> childResponses = child.ResponseIds;
-                foreach (var childresponse in childResponses)
-                {
-                  //  SurveyControlsRequest Request = new SurveyControlsRequest();
-                   // Request.SurveyId = childresponse.SurveyId;
-                    //SurveyControlsResponse ChildList = _isurveyFacade.GetSurveyControlList(Request);
-                    ResponseDetail childresponseDetail = new ResponseDetail();
-                    childresponseDetail.FormId = childresponse.SurveyId;
-                    childresponseDetail.ResponseId = childresponse.ResponseId;
-                    childresponseDetail.ParentResponseId = childresponse.RelateParentId;
-                    childresponseDetail.ParentFormId = childresponse.ParentRecordId;
-                    ResponseQA = new Dictionary<string, object>();
-                    ResponseQA.Add("FKEY", childresponse.RelateParentId);
-                    ResponseQA.Add("ResponseId", childresponse.ResponseId);
-                    var childsurveylist = List.Where(x => x.Key == childresponse.SurveyId).Select(d => d.Value).Single();
-
-                    XDocument xdochild = XDocument.Parse(childresponse.XML);
-                    int NumberOfPagesChild = GetNumberOfPags(childresponse.XML);
-                    for (int i = 1; NumberOfPagesChild + 1 > i; i++)
-                    {
-                        var _FieldsTypeIDs = from _FieldTypeID in
-                       xdochild.Descendants("Page")
-
-                                             where _FieldTypeID.Attribute("PageNumber").Value == (i).ToString()
-                                             select _FieldTypeID;
-
-                        var _PageFieldsTypeIDs = from _FieldTypeID1 in
-                                                     _FieldsTypeIDs.Descendants("ResponseDetail")
-
-                                                 select _FieldTypeID1;
-
-                        foreach (var item in _PageFieldsTypeIDs)
+                        try
                         {
-                            try
+                            string ControlId = item.Attribute("QuestionName").Value;
+                            bool IsCheckBox = (bool)surveylist.SurveyControlList.Any(x => x.ControlId == ControlId && x.ControlType == "CheckBox");
+                            bool ISNumericTextBox = (bool)surveylist.SurveyControlList.Any(x => x.ControlId == ControlId && x.ControlType == "NumericTextBox");
+                            if (ISNumericTextBox && !string.IsNullOrEmpty(item.Value))
                             {
-                                string ControlId = item.Attribute("QuestionName").Value;
-                                bool IsCheckBox = (bool)childsurveylist.SurveyControlList.Any(x => x.ControlId == ControlId && x.ControlType == "CheckBox");
-                                bool ISNumericTextBox = (bool)childsurveylist.SurveyControlList.Any(x => x.ControlId == ControlId && x.ControlType == "NumericTextBox");
-                                if (ISNumericTextBox && !string.IsNullOrEmpty(item.Value))
-                                {
-                                    string uiSep = CultureInfo.CurrentUICulture.NumberFormat.NumberDecimalSeparator;
-                                    if (item.Value.Contains(uiSep))
-                                        ResponseQA.Add(item.Attribute("QuestionName").Value, Convert.ToDecimal(item.Value));
-                                    else
-                                    ResponseQA.Add(item.Attribute("QuestionName").Value, Convert.ToInt32(item.Value));
-                                }
-                                else if (IsCheckBox)
-                                {
-                                    bool Ischecked = false;
-                                    if (item.Value == "Yes")
-                                        ResponseQA.Add(item.Attribute("QuestionName").Value, !Ischecked);
-                                    else
-                                        ResponseQA.Add(item.Attribute("QuestionName").Value, Ischecked);
-                                }
+                                string uiSep = CultureInfo.CurrentUICulture.NumberFormat.NumberDecimalSeparator;
+                                if (item.Value.Contains(uiSep))
+                                    RootResponseQA.Add(item.Attribute("QuestionName").Value, Convert.ToDecimal(item.Value));
                                 else
-                                    ResponseQA.Add(item.Attribute("QuestionName").Value, item.Value);
-                                ResponseQA.Add(item.Attribute("QuestionName").Value, item.Value);
+                                    RootResponseQA.Add(item.Attribute("QuestionName").Value, Convert.ToInt64(item.Value));
                             }
-                            catch (System.Exception ex)
+                            else if (IsCheckBox)
                             {
-
+                                bool Ischecked = false;
+                                if (item.Value == "Yes")
+                                    RootResponseQA.Add(item.Attribute("QuestionName").Value, !Ischecked);
+                                else if (item.Value == "No")
+                                    RootResponseQA.Add(item.Attribute("QuestionName").Value, Ischecked);
+                                else
+                                    RootResponseQA.Add(item.Attribute("QuestionName").Value, item.Value);
                             }
+                            else
+                                RootResponseQA.Add(item.Attribute("QuestionName").Value, item.Value);
+                        }
+                        catch(System.Exception ex)
+                        {
+                            RootResponseQA.Add(item.Attribute("QuestionName").Value, item.Value);
                         }
                     }
 
-                    childresponseDetail.ResponseQA = ResponseQA;
-                    Responsedetail.ChildResponseDetailList.Add(childresponseDetail);
                 }
+                Responsedetail.ResponseQA = RootResponseQA;
 
+                foreach (var child in ChildFormsHierarchy)
+                {
+                    List<SurveyAnswerDTO> childResponses = child.ResponseIds;
+                    foreach (var childresponse in childResponses)
+                    {
+                        ResponseDetail childresponseDetail = new ResponseDetail();
+                        childresponseDetail.FormId = childresponse.SurveyId;
+                        childresponseDetail.ResponseId = childresponse.ResponseId;
+                        childresponseDetail.ParentResponseId = childresponse.RelateParentId;
+                        childresponseDetail.ParentFormId = childresponse.ParentRecordId;
+                        ResponseQA = new Dictionary<string, object>();
+                        ResponseQA.Add("FKEY", childresponse.RelateParentId);
+                        ResponseQA.Add("ResponseId", childresponse.ResponseId);
+                        var childsurveylist = List.Where(x => x.Key == childresponse.SurveyId).Select(d => d.Value).Single();
+                        if (!string.IsNullOrEmpty(childresponse.XML))
+                        { 
+                            XDocument xdochild = XDocument.Parse(childresponse.XML);
+                            int NumberOfPagesChild = GetNumberOfPags(childresponse.XML);
+                            for (int i = 1; NumberOfPagesChild + 1 > i; i++)
+                            {
+                                var _FieldsTypeIDs = from _FieldTypeID in
+                               xdochild.Descendants("Page")
+                                                 where _FieldTypeID.Attribute("PageNumber").Value == (i).ToString()
+                                                 select _FieldTypeID;
+
+                                var _PageFieldsTypeIDs = from _FieldTypeID1 in
+                                                         _FieldsTypeIDs.Descendants("ResponseDetail")
+
+                                                     select _FieldTypeID1;
+                                foreach (var item in _PageFieldsTypeIDs)
+                                {
+                                    try
+                                    {
+                                        string ControlId = item.Attribute("QuestionName").Value;
+                                        bool IsCheckBox = (bool)childsurveylist.SurveyControlList.Any(x => x.ControlId == ControlId && x.ControlType == "CheckBox");
+                                        bool ISNumericTextBox = (bool)childsurveylist.SurveyControlList.Any(x => x.ControlId == ControlId && x.ControlType == "NumericTextBox");
+                                        if (ISNumericTextBox && !string.IsNullOrEmpty(item.Value))
+                                        {
+                                            string uiSep = CultureInfo.CurrentUICulture.NumberFormat.NumberDecimalSeparator;
+                                            if (item.Value.Contains(uiSep))
+                                                ResponseQA.Add(item.Attribute("QuestionName").Value, Convert.ToDecimal(item.Value));
+                                            else
+                                                ResponseQA.Add(item.Attribute("QuestionName").Value, Convert.ToInt64(item.Value));
+                                        }
+                                        else if (IsCheckBox)
+                                        {
+                                            bool Ischecked = false;
+                                            if (item.Value == "Yes")
+                                                ResponseQA.Add(item.Attribute("QuestionName").Value, !Ischecked);
+                                            else if (item.Value == "No")
+                                                ResponseQA.Add(item.Attribute("QuestionName").Value, Ischecked);
+                                            else
+                                                ResponseQA.Add(item.Attribute("QuestionName").Value, item.Value);
+                                        }
+                                        else
+                                            ResponseQA.Add(item.Attribute("QuestionName").Value, item.Value);                                        
+                                    }
+                                    catch (System.Exception ex)
+                                    {
+                                        ResponseQA.Add(item.Attribute("QuestionName").Value, item.Value);
+                                    }
+                                }
+                            }
+                        childresponseDetail.ResponseQA = ResponseQA;
+                        Responsedetail.ChildResponseDetailList.Add(childresponseDetail);
+                    }
+                  }               
+                }
+                 json = JsonConvert.SerializeObject(Responsedetail);
             }
-
-            var json = JsonConvert.SerializeObject(Responsedetail);
 
             return json;
         }
