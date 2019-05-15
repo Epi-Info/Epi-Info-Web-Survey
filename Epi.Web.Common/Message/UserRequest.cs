c15bbff50f533b4789c6f4bd22d0797be2d21e1d
mlamtahri <vwm5@cdc.gov>
2019-05-15 12:09:12 -0400

    Added missing files

---
+++ Epi.Web.Common/Message/UserRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+using Epi.Web.Common.MessageBase;
+using Epi.Web.Common.DTO;
+
+namespace Epi.Web.Common.Message
+    {
+   public class UserRequest
+        {
+       public UserRequest() 
+           {
+           this.Organization = new OrganizationDTO();
+           this.User = new UserDTO();
+           }
+       [DataMember]
+       public UserDTO User;
+       [DataMember]
+       public OrganizationDTO  Organization;
+       [DataMember]
+       public string Action;
+       [DataMember]
+       public int CurrentUser;
+       [DataMember]
+       public int CurrentOrg;
+       [DataMember]
+       public bool IsAuthenticated;
+        }
+    }
