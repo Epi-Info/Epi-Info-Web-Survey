fe8fe0bc7adcae9a86d57394ae4e9d7546686fc9
mlamtahri <vwm5@cdc.gov>
2019-05-16 10:55:45 -0400

    code clean up

---
+++ Epi.Web.Interfaces/DataInterface/IUserDao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Epi.Web.Common.BusinessObject;
+
+namespace Epi.Web.Interfaces.DataInterfaces
+{
+    public interface IUserDao
+    {
+       // UserBO GetUser(UserBO User);
+        bool GetExistingUser(UserBO User);
+        //bool UpdateUser(UserBO User);
+        //bool DeleteUser(UserBO User);
+        //bool InsertUser(UserBO User, OrganizationBO OrgBO);
+        //bool UpdateUserOrganization(UserBO User, OrganizationBO OrgBO);
+        //UserBO GetUserByUserId(UserBO User);
+        //bool UpdateUserPassword(UserBO User);
+        //bool UpdateUserInfo(UserBO User, OrganizationBO OrgBO);
+        //List<UserBO> GetUserByFormId(string FormId);
+        //UserBO GetCurrentUser(int userId);
+        //UserBO GetUserByEmail(UserBO User);
+        //bool IsUserExistsInOrganizaion(UserBO User, OrganizationBO OrgBO);
+        //List<UserBO> GetUserByOrgId(int OrgId);
+
+        //UserBO GetUserByUserIdAndOrgId(UserBO UserBO, OrganizationBO OrgBO);
+
+        //int GetUserHighestRole(int UserId);
+
+      //  List<UserBO> GetAdminsBySelectedOrgs(FormSettingBO FormSettingBO, string p);
+    }
+}
